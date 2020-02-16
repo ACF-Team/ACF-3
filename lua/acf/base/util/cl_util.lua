94820229e984347adc112ac8232af86c299f6522
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2020-02-15 22:43:16 -0300

    Added tool data functions
    - Clientside: Added ACF.WriteValue and ACF.Read[Bool/Number/String] functions to secure tool information and replace tool convars.
    - Clientside: Added PANEL:SetDataVariable and PANEL:ClearDataVariable to allow panels to control tool data variables.
    - Serverside: Added ACF.Read[Bool/Number/String] functions to retrieve tool data variables from the players on the server.

--- lua/acf/base/util/cl_util.lua
+++ lua/acf/base/util/cl_util.lua
@@ -1,71 +1,195 @@
-
-local Types = {
-	Normal = {
-		Prefix = "",
-		Color = Color(80, 255, 80)
-	},
-	Info = {
-		Prefix = " - Info",
-		Color = Color(0, 233, 255)
-	},
-	Warning = {
-		Prefix = " - Warning",
-		Color = Color(255, 160, 0)
-	},
-	Error = {
-		Prefix = " - Error",
-		Color = Color(255, 80, 80)
+do -- Clientside chat messages
+	local Types = {
+		Normal = {
+			Prefix = "",
+			Color = Color(80, 255, 80)
+		},
+		Info = {
+			Prefix = " - Info",
+			Color = Color(0, 233, 255)
+		},
+		Warning = {
+			Prefix = " - Warning",
+			Color = Color(255, 160, 0)
+		},
+		Error = {
+			Prefix = " - Error",
+			Color = Color(255, 80, 80)
+		}
 	}
-}
 
-function ACF.AddMessageType(Name, Prefix, TitleColor)
-	if not Name then return end
+	function ACF.AddMessageType(Name, Prefix, TitleColor)
+		if not Name then return end
 
-	Types[Name] = {
-		Prefix = Prefix and (" - " .. Prefix) or "",
-		Color = TitleColor or Color(80, 255, 80),
-	}
+		Types[Name] = {
+			Prefix = Prefix and (" - " .. Prefix) or "",
+			Color = TitleColor or Color(80, 255, 80),
+		}
+	end
+
+	local function PrintToChat(Type, ...)
+		if not ... then return end
+
+		local Data = Types[Type] or Types.Normal
+		local Prefix = "[ACF" .. Data.Prefix .. "] "
+		local Message = istable(...) and ... or { ... }
+
+		chat.AddText(Data.Color, Prefix, color_white, unpack(Message))
+	end
+
+	ACF.PrintToChat = PrintToChat
+
+	net.Receive("ACF_ChatMessage", function()
+		local Type = net.ReadString()
+		local Message = net.ReadTable()
+
+		PrintToChat(Type, Message)
+	end)
 end
 
-local function PrintToChat(Type, ...)
-	if not ... then return end
+do -- Custom fonts
+	surface.CreateFont("ACF_Title", {
+		font = "Roboto",
+		size = 22,
+		weight = 850,
+	})
 
-	local Data = Types[Type] or Types.Normal
-	local Prefix = "[ACF" .. Data.Prefix .. "] "
-	local Message = istable(...) and ... or { ... }
+	surface.CreateFont("ACF_Subtitle", {
+		font = "Roboto",
+		size = 18,
+		weight = 850,
+	})
 
-	chat.AddText(Data.Color, Prefix, color_white, unpack(Message))
+	surface.CreateFont("ACF_Paragraph", {
+		font = "Roboto",
+		size = 14,
+		weight = 650,
+	})
+
+	surface.CreateFont("ACF_Control", {
+		font = "Roboto",
+		size = 14,
+		weight = 550,
+	})
 end
 
-ACF.PrintToChat = PrintToChat
+do -- Tool data functions
+	local ToolData = {}
 
-net.Receive("ACF_ChatMessage", function()
-	local Type = net.ReadString()
-	local Message = net.ReadTable()
+	do -- Read functions
+		function ACF.ReadBool(Key)
+			if not Key then return false end
 
-	PrintToChat(Type, Message)
-end)
+			return tobool(ToolData[Key])
+		end
 
-surface.CreateFont("ACF_Title", {
-	font = "Roboto",
-	size = 23,
-	weight = 1000,
-})
+		function ACF.ReadNumber(Key)
+			if not Key then return 0 end
 
-surface.CreateFont("ACF_Subtitle", {
-	font = "Roboto",
-	size = 18,
-	weight = 1000,
-})
+			return tonumber(ToolData[Key])
+		end
 
-surface.CreateFont("ACF_Paragraph", {
-	font = "Roboto",
-	size = 14,
-	weight = 750,
-})
+		function ACF.ReadString(Key)
+			if not Key then return "" end
 
-surface.CreateFont("ACF_Control", {
-	font = "Roboto",
-	size = 14,
-	weight = 550,
-})
+			return tostring(ToolData[Key])
+		end
+	end
+
+	do -- Write function
+		local KeyPattern = "^[%w]+"
+		local ValuePattern = "[%w]*[%.]?[%w]+$"
+
+		local function IsValidKey(Key)
+			if not Key then return false end
+
+			return Key:match(KeyPattern) and true or false
+		end
+
+		local function IsValidValue(Value)
+			if not Value then return false end
+
+			return tostring(Value):match(ValuePattern) and true or false
+		end
+
+		local function UpdateKeyValue(Key, Value)
+			if ToolData[Key] == Value then return end
+
+			ToolData[Key] = Value
+
+			net.Start("ACF_ToolData")
+				net.WriteString(Key .. ":" .. Value)
+			net.SendToServer()
+
+			hook.Run("OnToolDataUpdate", Key, Value)
+
+			print("Sent", LocalPlayer(), Key, Value)
+		end
+
+		function ACF.WriteValue(Key, Value)
+			if not IsValidKey(Key) then return end
+			if not IsValidValue(Value) then return end
+
+			UpdateKeyValue(Key, Value)
+		end
+	end
+
+	do -- Panel functions
+		local PANEL = FindMetaTable("Panel")
+		local Panels = {}
+
+		local function AddFunctions(Panel)
+			Panel.LegacySetValue = Panel.SetValue
+			Panel.LegacyRemove = Panel.Remove
+
+			Panels[Panel] = true
+
+			function Panel:SetValue(Value, ...)
+				ACF.WriteValue(self.DataVar, Value)
+
+				self:LegacySetValue(Value, ...)
+			end
+
+			function Panel:Remove(...)
+				Panels[Panel] = nil
+
+				self:LegacyRemove(...)
+			end
+		end
+
+		function PANEL:SetDataVariable(Key)
+			if not Key then return end
+			if not self.SetValue then return end
+
+			self.DataVar = Key
+
+			if not ToolData[Key] then
+				ACF.WriteValue(Key, self:GetValue())
+			end
+
+			if not self.LegacySetValue then
+				AddFunctions(self)
+			end
+		end
+
+		function PANEL:ClearDataVariable()
+			if not self.LegacySetValue then return end
+
+			Panels[self] = nil
+
+			self.SetValue = self.LegacySetValue
+			self.Remove = self.LegacyRemove
+			self.LegacySetValue = nil
+			self.LegacyRemove = nil
+			self.DataVar = nil
+		end
+
+		hook.Add("OnToolDataUpdate", "ACF Update Panel Values", function(Key, Value)
+			for Panel in pairs(Panels) do
+				if Panel.DataVar == Key then
+					Panel:LegacySetValue(Value)
+				end
+			end
+		end)
+	end
+end
--- lua/acf/base/util/sv_util.lua
+++ lua/acf/base/util/sv_util.lua
@@ -1,58 +1,115 @@
-util.AddNetworkString("ACF_ChatMessage")
+do -- Serverside console log messages
+	local Types = {
+		Normal = {
+			Prefix = "",
+			Color = Color(80, 255, 80)
+		},
+		Info = {
+			Prefix = " - Info",
+			Color = Color(0, 233, 255)
+		},
+		Warning = {
+			Prefix = " - Warning",
+			Color = Color(255, 160, 0)
+		},
+		Error = {
+			Prefix = " - Error",
+			Color = Color(255, 80, 80)
+		}
+	}
 
-function ACF.SendMessage(Player, Type, ...)
-	if not ... then return end
+	function ACF.AddLogType(Name, Prefix, TitleColor)
+		if not Name then return end
 
-	local Message = istable(...) and ... or { ... }
+		Types[Name] = {
+			Prefix = Prefix and (" - " .. Prefix) or "",
+			Color = TitleColor or Color(80, 255, 80),
+		}
+	end
 
-	net.Start("ACF_ChatMessage")
-		net.WriteString(Type or "Normal")
-		net.WriteTable(Message)
-	if IsValid(Player) then
-		net.Send(Player)
-	else
-		net.Broadcast()
+	function ACF.PrintLog(Type, ...)
+		if not ... then return end
+
+		local Data = Types[Type] or Types.Normal
+		local Prefix = "[ACF" .. Data.Prefix .. "] "
+		local Message = istable(...) and ... or { ... }
+
+		Message[#Message + 1] = "\n"
+
+		MsgC(Data.Color, Prefix, color_white, unpack(Message))
 	end
 end
 
-local Types = {
-	Normal = {
-		Prefix = "",
-		Color = Color(80, 255, 80)
-	},
-	Info = {
-		Prefix = " - Info",
-		Color = Color(0, 233, 255)
-	},
-	Warning = {
-		Prefix = " - Warning",
-		Color = Color(255, 160, 0)
-	},
-	Error = {
-		Prefix = " - Error",
-		Color = Color(255, 80, 80)
-	}
-}
+do -- Clientside message delivery
+	util.AddNetworkString("ACF_ChatMessage")
 
-function ACF.AddLogType(Name, Prefix, TitleColor)
-	if not Name then return end
+	function ACF.SendMessage(Player, Type, ...)
+		if not ... then return end
 
-	Types[Name] = {
-		Prefix = Prefix and (" - " .. Prefix) or "",
-		Color = TitleColor or Color(80, 255, 80),
-	}
+		local Message = istable(...) and ... or { ... }
+
+		net.Start("ACF_ChatMessage")
+			net.WriteString(Type or "Normal")
+			net.WriteTable(Message)
+		if IsValid(Player) then
+			net.Send(Player)
+		else
+			net.Broadcast()
+		end
+	end
 end
 
-function ACF.PrintLog(Type, ...)
-	if not ... then return end
+do -- Tool data functions
+	local ToolData = {}
 
-	local Data = Types[Type] or Types.Normal
-	local Prefix = "[ACF" .. Data.Prefix .. "] "
-	local Message = istable(...) and ... or { ... }
+	do -- Data syncronization
+		util.AddNetworkString("ACF_ToolData")
 
-	Message[#Message + 1] = "\n"
+		local function TranslateData(String)
+			return unpack(string.Explode(":", String))
+		end
 
-	MsgC(Data.Color, Prefix, color_white, unpack(Message))
+		net.Receive("ACF_ToolData", function(_, Player)
+			if not IsValid(Player) then return end
+
+			local Key, Value = TranslateData(net.ReadString())
+
+			ToolData[Player][Key] = Value
+
+			print("Received", Player, Key, Value)
+		end)
+
+		hook.Add("PlayerInitialSpawn", "ACF Tool Data", function(Player)
+			ToolData[Player] = {}
+		end)
+
+		hook.Add("PlayerDisconnected", "ACF Tool Data", function(Player)
+			ToolData[Player] = nil
+		end)
+	end
+
+	do -- Read functions
+		function ACF.ReadBool(Player, Key)
+			if not Key then return false end
+			if not ToolData[Player] then return false end
+
+			return tobool(ToolData[Player][Key])
+		end
+
+		function ACF.ReadNumber(Player, Key)
+			if not Key then return 0 end
+			if not ToolData[Player] then return 0 end
+
+			return tonumber(ToolData[Player][Key])
+		end
+
+		function ACF.ReadString(Player, Key)
+			if not Key then return "" end
+			if not ToolData[Player] then return "" end
+
+			return tostring(ToolData[Player][Key])
+		end
+	end
 end
 
 function ACF_GetHitAngle(HitNormal, HitVector)
