71e3e93f9a1d4859ead43c23471dd4b2b8e01470
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2020-02-21 01:09:45 -0300

    Added Classes library (taken from ACF-3 Missiles)

--- lua/acf/base/sh_classes.lua
+++ lua/acf/base/sh_classes.lua
@@ -1,4 +1,85 @@
-do -- Weapon class registration functions
+do -- Class registration function
+	local Classes = {}
+	local Queued = {}
+
+	local function CreateInstance(Class)
+		local New = {}
+
+		setmetatable(New, { __index = Class })
+
+		if New.OnCalled then
+			New:OnCalled()
+		end
+
+		return New
+	end
+
+	local function QueueBaseClass(Name, Base)
+		if not Queued[Base] then
+			Queued[Base] = { [Name] = true }
+		else
+			Queued[Base][Name] = true
+		end
+	end
+
+	local function AttachMetaTable(Class, Name, Base)
+		local OldMeta = getmetatable(Class) or {}
+
+		if Base then
+			local BaseClass = Classes[Base]
+
+			if BaseClass then
+				Class.BaseClass = BaseClass
+				OldMeta.__index = BaseClass
+			else
+				QueueBaseClass(Name, Base)
+			end
+		end
+
+		OldMeta.__call = function()
+			return CreateInstance(Class)
+		end
+
+		setmetatable(Class, OldMeta)
+
+		timer.Simple(0, function()
+			if Class.OnLoaded then
+				Class:OnLoaded()
+			end
+		end)
+	end
+
+	function ACF.RegisterClass(Name, Base, Destiny)
+		if not Classes[Name] then
+			Classes[Name] = {}
+		end
+
+		local Class = Classes[Name]
+		Class.Name = Name
+
+		AttachMetaTable(Class, Name, Base)
+
+		if Queued[Name] then
+			local Current
+
+			for K in pairs(Queued[Name]) do
+				Current = Classes[K]
+
+				AttachMetaTable(Current, Current.Name, Name)
+			end
+
+			Queued[Name] = nil
+		end
+
+		if Destiny then
+			Destiny[Name] = Class
+		end
+
+		return Class
+	end
+end
+
+do -- Weapon registration functions
 	ACF.Classes.Weapons = ACF.Classes.Weapons or {}
 
 	local Weapons = ACF.Classes.Weapons
