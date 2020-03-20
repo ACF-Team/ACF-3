3d86cbefd6ff85dcf4ba1fbd4c2f51ccebc3995c
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2020-03-20 03:10:56 -0300

    Added fuel tank selection to engine menu
    - Engine menu now has the controls needed to select a fuel tank.
    - Adapted existing controls of the engine menu to the latest class related changes.

--- lua/acf/client/menu_items/engines_menu.lua
+++ lua/acf/client/menu_items/engines_menu.lua
@@ -1,7 +1,7 @@
+local EngineTypes = ACF.Classes.EngineTypes
+local FuelTypes = ACF.Classes.FuelTypes
 local FuelTanks = ACF.Classes.FuelTanks
 local Engines = ACF.Classes.Engines
-local FuelDensity = ACF.FuelDensity
-local Efficiency = ACF.Efficiency
 local Selected = {}
 local Sorted = {}
 
@@ -40,7 +40,7 @@ local function UpdateEngineStats(Label, Data)
 	local MaxPower = RPM.PeakMax
 
 	local RPMText = "Idle RPM : %s RPM\nPowerband : %s-%s RPM\nRedline : %s RPM\nMass : %s"
-	local Text = ""
+	local LabelText = ""
 
 	-- Electric motors and turbines get peak power in middle of rpm range
 	if Data.IsElectric then
@@ -50,34 +50,32 @@ local function UpdateEngineStats(Label, Data)
 		MaxPower = PeakkWRPM
 	end
 
-	Text = Text .. RPMText:format(RPM.Idle, MinPower, MaxPower, RPM.Limit, ACF.GetProperMass(Data.Mass))
+	LabelText = LabelText .. RPMText:format(RPM.Idle, MinPower, MaxPower, RPM.Limit, ACF.GetProperMass(Data.Mass))
 
 	local TorqueText = "\nPeak Torque : %s n/m - %s ft-lb"
 	local PowerText = "\nPeak Power : %s kW - %s HP @ %s RPM"
-	local FuelText = "\n\nFuel Type : %s%s%s"
 	local Consumption, Power = "", ""
 	local Boost = Data.RequiresFuel and ACF.TorqueBoost or 1
-	local Fuel = Data.Fuel
 
-	if Fuel == "Electric" then
-		local ElecText = "\nPeak Energy Consumption :\n%s kW - %s MJ/min"
-		local ElecRate = ACF.ElecRate * PeakkW / Efficiency[Data.Type]
+	for K in pairs(Data.Fuel) do
+		if not FuelTypes[K] then continue end
 
-		Consumption = ElecText:format(math.Round(ElecRate, 2), math.Round(ElecRate * 0.06, 2))
-	else
-		local ConsumptionText = "\n%s Consumption :\n%s L/min - %s gal/min @ %s RPM"
+		local Type = EngineTypes[Data.Type]
+		local Fuel = FuelTypes[K]
+		local AddText = ""
 
-		if Fuel == "Multifuel" or Fuel == "Diesel" then
-			local FuelRate = ACF.FuelRate * Efficiency[Data.Type] * ACF.TorqueBoost * PeakkW / (60 * FuelDensity.Diesel)
+		if Fuel.ConsumptionText then
+			AddText = Fuel.ConsumptionText(PeakkW, PeakkWRPM, Type, Fuel)
+		else
+			local Text = "\n\n%s Consumption :\n%s L/min - %s gal/min @ %s RPM"
+			local Rate = ACF.FuelRate * Type.Efficiency * ACF.TorqueBoost * PeakkW / (60 * Fuel.Density)
 
-			Consumption = Consumption .. ConsumptionText:format("Diesel", math.Round(FuelRate, 2), math.Round(FuelRate * 0.264, 2), PeakkWRPM)
+			AddText = Text:format(Fuel.Name, math.Round(Rate, 2), math.Round(Rate * 0.264, 2), PeakkWRPM)
 		end
 
-		if Fuel == "Multifuel" or Fuel == "Petrol" then
-			local FuelRate = ACF.FuelRate * Efficiency[Data.Type] * ACF.TorqueBoost * PeakkW / (60 * FuelDensity.Petrol)
+		Consumption = Consumption .. AddText
 
-			Consumption = Consumption .. ConsumptionText:format("Petrol", math.Round(FuelRate, 2), math.Round(FuelRate * 0.264, 2), PeakkWRPM)
-		end
+		Data.Fuel[K] = Fuel -- Replace once engines use the proper class functions
 	end
 
 	Power = Power .. "\n" .. PowerText:format(math.floor(PeakkW * Boost), math.floor(PeakkW * Boost * 1.34), PeakkWRPM)
@@ -90,9 +88,9 @@ local function UpdateEngineStats(Label, Data)
 		Power = Power .. TorqueText:format(math.floor(Data.Torque * ACF.TorqueBoost), math.floor(Data.Torque * ACF.TorqueBoost * 0.73))
 	end
 
-	Text = Text .. FuelText:format(Fuel, Consumption, Power)
+	LabelText = LabelText .. Consumption .. Power
 
-	Label:SetText(Text)
+	Label:SetText(LabelText)
 end
 
 local function CreateMenu(Menu)
@@ -102,7 +100,12 @@ local function CreateMenu(Menu)
 	local EngineDesc = Menu:AddLabel()
 	local EngineStats = Menu:AddLabel()
 
+	Menu:AddTitle("Fuel Settings")
+
+	local FuelClass = Menu:AddComboBox()
 	local FuelList = Menu:AddComboBox()
+	local FuelType = Menu:AddComboBox()
+	local FuelDesc = Menu:AddLabel()
 
 	ACF.WriteValue("PrimaryClass", "acf_engine")
 	ACF.WriteValue("SecondaryClass", "acf_fueltank")
@@ -136,10 +139,89 @@ local function CreateMenu(Menu)
 		EngineDesc:SetText((ClassDesc and (ClassDesc .. "\n\n") or "") .. Data.Description)
 
 		UpdateEngineStats(EngineStats, Data)
+
+		LoadSortedList(FuelType, Data.Fuel, "ID")
+	end
+
+	function FuelClass:OnSelect(Index, _, Data)
+		if self.Selected == Data then return end
+
+		self.Selected = Data
+
+		local Choices = Sorted[FuelTanks]
+		Selected[Choices] = Index
+
+		LoadSortedList(FuelList, Data.Items, "ID")
+	end
+
+	function FuelList:OnSelect(Index, _, Data)
+		if self.Selected == Data then return end
+
+		self.Selected = Data
+
+		local ClassData = FuelClass.Selected
+		local ClassDesc = ClassData.Description
+		local Choices = Sorted[ClassData.Items]
+		Selected[Choices] = Index
+
+		self.Description = (ClassDesc and (ClassDesc .. "\n\n") or "") .. Data.Description
+
+		ACF.WriteValue("FuelTank", Data.ID)
+
+		FuelType:UpdateFuelText()
+	end
+
+	function FuelType:OnSelect(Index, _, Data)
+		if self.Selected == Data then return end
+
+		self.Selected = Data
+
+		local ClassData = EngineList.Selected
+		local Choices = Sorted[ClassData.Fuel]
+		Selected[Choices] = Index
+
+		ACF.WriteValue("FuelType", Data.ID)
+
+		self:UpdateFuelText()
+	end
+
+	function FuelType:UpdateFuelText()
+		if not self.Selected then return end
+		if not FuelList.Selected then return end
+
+		local FuelTank = FuelList.Selected
+		local FuelText = FuelList.Description
+		local TextFunc = self.Selected.FuelTankText
+
+		local Wall		= 0.03937 --wall thickness in inches (1mm)
+		local Volume	= FuelTank.Volume - (FuelTank.SurfaceArea * Wall) -- total volume of tank (cu in), reduced by wall thickness
+		local Capacity	= Volume * ACF.CuIToLiter * ACF.TankVolumeMul * 0.4774 --internal volume available for fuel in liters, with magic realism number
+		local EmptyMass	= FuelTank.SurfaceArea * Wall * 16.387 * 0.0079 -- total wall volume * cu in to cc * density of steel (kg/cc)
+		local Mass		= EmptyMass + Capacity * self.Selected.Density -- weight of tank + weight of fuel
+
+		if TextFunc then
+			FuelText = FuelText .. TextFunc(Capacity, Mass, EmptyMass)
+		else
+			local Text = "\n\nCapacity : %s L - %s gal\nFull Mass : %s\nEmpty Mass : %s"
+			local Liters = math.Round(Capacity, 2)
+			local Gallons = math.Round(Capacity * 0.264172, 2)
+
+			FuelText = FuelText .. Text:format(Liters, Gallons, ACF.GetProperMass(Mass), ACF.GetProperMass(EmptyMass))
+		end
+
+		if not FuelTank.IsExplosive then
+			FuelText = FuelText .. "\n\nThis fuel tank won't explode if damaged."
+		end
+
+		if FuelTank.Unlinkable then
+			FuelText = FuelText .. "\n\nThis fuel tank cannot be linked to other ACF entities."
+		end
+
+		FuelDesc:SetText(FuelText)
 	end
 
 	LoadSortedList(EngineClass, Engines, "ID")
-	LoadSortedList(FuelList, FuelTanks, "ID")
+	LoadSortedList(FuelClass, FuelTanks, "ID")
 end
 
 ACF.AddOptionItem("Entities", "Engines", "car", CreateMenu)
