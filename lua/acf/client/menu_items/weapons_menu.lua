bebdb555b5bc68ffdf64c86e86923d51619d0a1c
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2020-04-28 00:41:09 -0400

    Update weapon menu's rate of fire display
    - The weapon menu now uses the correct formula to display a weapon's rate of fire.
    - The rate of fire will be updated on the menu according to ammo type changes for non cyclic weapons.

--- lua/acf/client/menu_items/weapons_menu.lua
+++ lua/acf/client/menu_items/weapons_menu.lua
@@ -53,6 +53,7 @@ end
 local function CreateMenu(Menu)
 	local EntText = "Mass : %s kg\nFirerate : %s rpm\nSpread : %s degrees%s"
 	local MagText = "\nRounds : %s rounds\nReload : %s seconds"
+	local AmmoData
 
 	local ClassList = Menu:AddComboBox()
 	local EntList = Menu:AddComboBox()
@@ -92,20 +93,32 @@ local function CreateMenu(Menu)
 		self.Selected = Data
 
 		local ClassData = ClassList.Selected
-		local RoundVolume = 3.1416 * (Data.Caliber * 0.05) ^ 2 * Data.Round.MaxLength
-		local Firerate = Data.Cyclic or 60 / (((RoundVolume * 0.002) ^ 0.6) * ClassData.ROFMod * (Data.ROFMod or 1))
-		local Magazine = Data.MagSize and MagText:format(Data.MagSize, Data.MagReload) or ""
-
 		local Choices = Sorted[ClassData.Items]
 		Selected[Choices] = Index
 
 		ACF.WriteValue("Weapon", Data.ID)
 
 		EntName:SetText(Data.Name)
-		EntData:SetText(EntText:format(Data.Mass, math.Round(Firerate, 2), ClassData.Spread * 100, Magazine))
 
 		AmmoList:UpdateMenu()
 	end
+
+	EntData:TrackDataVar("Projectile", "SetText")
+	EntData:TrackDataVar("Propellant")
+	EntData:TrackDataVar("Tracer")
+	EntData:SetValueFunction(function()
+		local Class = ClassList.Selected
+		local Data = EntList.Selected
+
+		if not Class then return "" end
+		if not Data then return "" end
+
+		local ReloadTime = AmmoData and (ACF.BaseReload + (AmmoData.ProjMass + AmmoData.PropMass) * ACF.MassToTime) or 60
+		local Firerate = Data.Cyclic or 60 / ReloadTime
+		local Magazine = Data.MagSize and MagText:format(Data.MagSize, Data.MagReload) or ""
+
+		return EntText:format(Data.Mass, math.Round(Firerate, 2), Class.Spread * 100, Magazine)
+	end)
 
 	function CrateList:OnSelect(Index, _, Data)
 		if self.Selected == Data then return end
@@ -136,7 +149,8 @@ local function CreateMenu(Menu)
 
 		local Ammo = self.Selected
 		local ToolData = Ammo:GetToolData()
-		local Data = Ammo:ClientConvert(Menu, ToolData)
+
+		AmmoData = Ammo:ClientConvert(Menu, ToolData)
 
 		Menu:ClearTemporal(self)
 		Menu:StartTemporal(self)
@@ -150,51 +164,51 @@ local function CreateMenu(Menu)
 			RoundLength:TrackDataVar("Tracer")
 			RoundLength:SetValueFunction(function()
 				local Text = "Round Length: %s / %s cm"
-				local CurLength = Data.ProjLength + Data.PropLength + Data.Tracer
-				local MaxLength = Data.MaxRoundLength
+				local CurLength = AmmoData.ProjLength + AmmoData.PropLength + AmmoData.Tracer
+				local MaxLength = AmmoData.MaxRoundLength
 
 				return Text:format(CurLength, MaxLength)
 			end)
 
-			local Projectile = Menu:AddSlider("Projectile Length", 0, Data.MaxRoundLength, 2)
+			local Projectile = Menu:AddSlider("Projectile Length", 0, AmmoData.MaxRoundLength, 2)
 			Projectile:SetDataVar("Projectile", "OnValueChanged")
 			Projectile:SetValueFunction(function(Panel, IsTracked)
 				ToolData.Projectile = ACF.ReadNumber("Projectile")
 
 				if not IsTracked then
-					Data.Priority = "Projectile"
+					AmmoData.Priority = "Projectile"
 				end
 
-				Ammo:UpdateRoundData(ToolData, Data)
+				Ammo:UpdateRoundData(ToolData, AmmoData)
 
-				ACF.WriteValue("Propellant", Data.PropLength)
+				ACF.WriteValue("Propellant", AmmoData.PropLength)
 
-				Panel:SetValue(Data.ProjLength)
+				Panel:SetValue(AmmoData.ProjLength)
 
-				return Data.ProjLength
+				return AmmoData.ProjLength
 			end)
 
-			local Propellant = Menu:AddSlider("Propellant Length", 0, Data.MaxRoundLength, 2)
+			local Propellant = Menu:AddSlider("Propellant Length", 0, AmmoData.MaxRoundLength, 2)
 			Propellant:SetDataVar("Propellant", "OnValueChanged")
 			Propellant:SetValueFunction(function(Panel, IsTracked)
 				ToolData.Propellant = ACF.ReadNumber("Propellant")
 
 				if not IsTracked then
-					Data.Priority = "Propellant"
+					AmmoData.Priority = "Propellant"
 				end
 
-				Ammo:UpdateRoundData(ToolData, Data)
+				Ammo:UpdateRoundData(ToolData, AmmoData)
 
-				ACF.WriteValue("Projectile", Data.ProjLength)
+				ACF.WriteValue("Projectile", AmmoData.ProjLength)
 
-				Panel:SetValue(Data.PropLength)
+				Panel:SetValue(AmmoData.PropLength)
 
-				return Data.PropLength
+				return AmmoData.PropLength
 			end)
 		end
 
 		if Ammo.MenuAction then
-			Ammo:MenuAction(Menu, ToolData, Data)
+			Ammo:MenuAction(Menu, ToolData, AmmoData)
 		end
 
 		Menu:EndTemporal(self)
