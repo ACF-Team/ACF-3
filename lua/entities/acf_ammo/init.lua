c31c9486b7ac2b99e5f9d772f1e259f060468260
LiddulBOFH <13317534+LiddulBOFH@users.noreply.github.com>
2020-05-12 00:22:03 -0400

    Greatly improved ammo capacity calculation
    Rounds are now actually stored based on size, instead of the messy soup that was before
    Unfortunately due to unsalted spaghetti, ACF-Missiles doesn't play nice so its still calculated using the old method
    You WILL have to go through your vehicles and update/tweak ammo, you may find you can store more, or you may find that you can't store any at all
    There are also some unused inputs (AddSpacing and AddArmor) on the new function that can potentially affect ammoboxes in the future
    Spacing will make the rounds spread a bit more, this can (not yet) be used to lower the chances of an ammo rack explosion, at the cost of ammo storage
    Armor is obvious, except its internal, and will also directly reduce ammo storage
    This also paves the way for scalable ammo boxes!
    
    Also a typo fixed

--- lua/entities/acf_ammo/init.lua
+++ lua/entities/acf_ammo/init.lua
@@ -89,6 +89,65 @@ local function RefillCrates(Entity)
 	end
 end
 
+-- BoxSize is just OBBMaxs-OBBMins
+-- RoundCaliber is obvious, but must be in cm (as is stored)
+-- TotalRoundLength takes into account the entire projectile, trace and propellant included
+-- AddSpacing is just extra spacing (directly reduces storage, but can later make it harder to detonate)
+-- AddArmor is literally just extra armor on the ammo crate, but inside (also directly reduces storage)
+local function CalcAmmo(BoxSize,RoundCaliber,TotalRoundLength,AddSpacing,AddArmor)
+	-- Instantly invalidate garbage rounds
+	if RoundCaliber==0 then return 0 end
+	if TotalRoundLength==0 then return 0 end
+
+	local Rounds=0
+	-- Converting everything to source units
+	local ConvCaliber=(RoundCaliber/0.75)/2.54
+	local ConvLength=(TotalRoundLength/0.75)/2.54
+	print(ConvCaliber,ConvLength)
+	local Spacing=math.max(math.abs(AddSpacing)+0.125,0.125)
+
+	if AddArmor>0 then
+		local ConvArmor=(AddArmor/0.75)/25.4
+		-- *2 because armor on both sides
+		BoxSize={x=math.max(BoxSize.x-(ConvArmor*2),0), y=math.max(BoxSize.y-(ConvArmor*2),0), z=math.max(BoxSize.z-(ConvArmor*2),0)}
+	end
+
+	local D={}
+	D["x"]=BoxSize.x
+	D["y"]=BoxSize.y
+	D["z"]=BoxSize.z
+	for K,V in pairs(D) do
+		if ShortestFit==nil then
+			if ConvLength<=V then ShortestFit=K end
+		else
+			if ConvLength<=V and ConvLength>(D[ShortestFit]) then ShortestFit=K end
+		end
+	end
+
+	if ShortestFit~="" then -- From here we know the round can sorta fit in the box
+		local X=0
+		local Y=0
+		-- Creating the 'plane' to do the basic bitch math with
+		if ShortestFit=="x" then
+			X=D["y"]
+			Y=D["z"]
+		elseif ShortestFit=="y" then
+			X=D["x"]
+			Y=D["z"]
+		else -- z
+			X=D["x"]
+			Y=D["y"]
+		end
+
+		local ModifiedRoundSize=ConvCaliber+Spacing
+		local ModifiedRoundLength=ConvLength+Spacing
+		-- That basic bitch math
+		Rounds=math.floor(D[ShortestFit]/ModifiedRoundLength)*math.floor(X/ModifiedRoundSize)*math.floor(Y/ModifiedRoundSize)
+	end
+
+	return Rounds
+end
+
 local function UpdateAmmoData(Entity, Data1, Data2, Data3, Data4, Data5, Data6, Data7, Data8, Data9, Data10)
 	local GunData = ACF.Weapons.Guns[Data1]
 
@@ -103,8 +162,8 @@ local function UpdateAmmoData(Entity, Data1, Data2, Data3, Data4, Data5, Data6, 
 	--Data 1 to 4 are should always be Round ID, Round Type, Propellant lenght, Projectile lenght
 	Entity.RoundId = Data1 --Weapon this round loads into, ie 140mmC, 105mmH ...
 	Entity.RoundType = RoundData and Data2 or "AP" --Type of round, IE AP, HE, HEAT ...
-	Entity.RoundPropellant = Data3 --Lenght of propellant
-	Entity.RoundProjectile = Data4 --Lenght of the projectile
+	Entity.RoundPropellant = Data3 --Length of propellant
+	Entity.RoundProjectile = Data4 --Length of the projectile
 	Entity.RoundData5 = Data5 or 0
 	Entity.RoundData6 = Data6 or 0
 	Entity.RoundData7 = Data7 or 0
@@ -159,8 +218,20 @@ local function UpdateAmmoData(Entity, Data1, Data2, Data3, Data4, Data5, Data6, 
 	local MassMod = Entity.BulletData.MassMod or 1
 
 	Entity.Volume = Volume * Efficiency
-	Entity.Capacity = math.floor(CapMul * Entity.Volume * 16.38 / Entity.BulletData.RoundVolume)
-	Entity.AmmoMassMax = math.floor((Entity.BulletData.ProjMass * MassMod + Entity.BulletData.PropMass) * Entity.Capacity * 2) -- why *2 ?
+	--Entity.Capacity = math.floor(CapMul * Entity.Volume * 16.38 / Entity.BulletData.RoundVolume)
+	-- CalcAmmo function is just above
+	if (GunData.ent or "")=="acf_rack" then -- I hate everything that was involved for me to have to do this, because '30000'
+		-- IF, AND ONLY THEN, ACF-MISSILES GETS UNFUCKED
+		-- Make sure caliber is in cm, and length is in cm
+		-- When all is said and done, this horrible line can be replaced with the new function
+		-- I want the missiles/bombs to be stored like the entire fucking model is in there
+		Entity.Capacity = math.floor(CapMul * Entity.Volume * 16.38 / Entity.BulletData.RoundVolume)
+	else
+		print(GunData.caliber)
+		Entity.Capacity = CalcAmmo(Entity:OBBMaxs()-Entity:OBBMins(), GunData.caliber or 0,((Entity.BulletData.PropLength or 0) + (Entity.BulletData.ProjLength or 0) + (Entity.BulletData.Tracer or 0)), 0, 0)
+	end
+	if Entity.RoundType == "Refill" then Entity.Capacity=CalcAmmo(Entity:OBBMaxs()-Entity:OBBMins(),8,2,0,0) end
+	Entity.AmmoMassMax = math.floor((Entity.BulletData.ProjMass + Entity.BulletData.PropMass) * Entity.Capacity)
 	Entity.Caliber = GunData.caliber
 	Entity.RoFMul = (Volume > 27000) and (1 - (math.log(Volume * 0.00066) / math.log(2) - 4) * 0.2) or 1 --*0.0625 for 25% @ 4x8x8, 0.025 10%, 0.0375 15%, 0.05 20% --0.23 karb edit for cannon rof 2. changed to start from 2x3x4 instead of 2x4x4
 	Entity.Spread = GunClass.spread * ACF.GunInaccuracyScale
