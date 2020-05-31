d93a212936772930a521a4bba4f0dbd9183ff120
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2020-05-31 07:59:09 -0400

    Added clientside ACF Trace and Visclip check
    - Added a clientside copy of the ACF.Trace and ACF_CheckClips functions.

--- lua/acf/client/cl_ballistics.lua
+++ lua/acf/client/cl_ballistics.lua
@@ -1,5 +1,52 @@
 ACF.BulletEffect = ACF.BulletEffect or {}
 
+local TraceLine = util.TraceLine
+
+local function HitClip(Ent, Pos)
+	if not IsValid(Ent) then return false end
+	if Ent.ClipData == nil then return false end -- Doesn't have clips
+	if Ent:GetClass() ~= "prop_physics" then return false end -- Only care about props
+
+	local Center = Ent:LocalToWorld(Ent:OBBCenter())
+
+	for I = 1, #Ent.ClipData do
+		local Clip 	 = Ent.ClipData[I]
+		local Normal = Ent:LocalToWorldAngles(Clip[1]):Forward()
+		local Origin = Center + Normal * Clip[2]
+
+		if Normal:Dot((Origin - Pos):GetNormalized()) > 0 then return true end
+	end
+
+	return false
+end
+
+local function Trace(TraceData, Filter) -- Pass true on filter to have Trace make it's own copy of TraceData.filter to modify
+	if Filter == true then
+		Filter = TraceData.filter
+		local NewFilter = {}
+
+		for I = 1, #Filter do
+			NewFilter[I] = Filter[I]
+		end
+
+		TraceData.filter = NewFilter
+	end
+
+	local T = TraceLine(TraceData)
+
+	if T.HitNonWorld and HitClip(T.Entity, T.HitPos) then
+		TraceData.filter[#TraceData.filter + 1] = T.Entity
+
+		return Trace(TraceData, Filter)
+	end
+
+	if Filter then
+		TraceData.filter = Filter
+	end
+
+	return T
+end
+
 local function BulletFlight(Bullet)
 	local DeltaTime = CurTime() - Bullet.LastThink
 	local Drag = Bullet.SimFlight:GetNormalized() *  (Bullet.DragCoef * Bullet.SimFlight:Length() ^ 2 ) / ACF.DragDiv
@@ -24,3 +71,5 @@ hook.Add("Think", "ACF_ManageBulletEffects", function()
 end)
 
 ACF_SimBulletFlight = BulletFlight
+ACF_CheckClips = HitClip
+ACF.Trace = Trace
