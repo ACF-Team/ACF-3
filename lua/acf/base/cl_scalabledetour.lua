437057a7db0a7a6067da31e95e454142bb3dc7f3
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2020-08-12 03:23:55 -0400

    Fixed crates disappearing with Visual Clip
    - Fixed scalable crates disappearing when looked at while using the Visual Clip tool.
    - Added extra varargs at the end of the detoured functions to prevent future conflicts with other addons that also override these functions.

--- lua/acf/base/cl_scalabledetour.lua
+++ lua/acf/base/cl_scalabledetour.lua
@@ -2,7 +2,7 @@ hook.Add("Initialize", "Scalable Entities", function()
 	do -- Detour decals to scale better on scaled entities
 		local DecalEx = util.DecalEx
 
-		util.DecalEx = function(Mat, Ent, Pos, Normal, Color, W, H)
+		util.DecalEx = function(Mat, Ent, Pos, Normal, Color, W, H, ...)
 			if Ent.IsScalable and Ent:GetSize() then -- If entity is scaled, offset decal pos
 				local Offset = Pos - Ent:GetPos()
 
@@ -20,29 +20,31 @@ hook.Add("Initialize", "Scalable Entities", function()
 				H = H * Max
 			end
 
-			DecalEx(Mat, Ent, Pos, Normal, Color, W, H)
+			DecalEx(Mat, Ent, Pos, Normal, Color, W, H, ...)
 		end
 	end
 
 	do -- Dealing with visual clip's bullshit
 		local Meta = FindMetaTable("Entity")
-		local EnaMatr = Meta.EnableMatrix
-		local DisMatr = Meta.DisableMatrix
+		local EnableMatrix = Meta.EnableMatrix
+		local DisableMatrix = Meta.DisableMatrix
 
-		function Meta:EnableMatrix(Str, NewScale)
+		function Meta:EnableMatrix(Str, NewScale, ...)
 			if self.IsScalable and self:GetSize() and Str == "RenderMultiply" then
 				local Size  = self:GetOriginalSize()
 				local Scale = Vector(1 / Size.x, 1 / Size.y, 1 / Size.z) * self:GetSize()
 
-				if NewScale:GetScale() == Scale then
-					return
+				-- Visual clip provides a scale of 0, 0, 0
+				-- So we just update it with our actual scale
+				if NewScale:GetScale() ~= Scale then
+					NewScale:SetScale(Scale)
 				end
 			end
 
-			EnaMatr(self, Str, NewScale)
+			EnableMatrix(self, Str, NewScale, ...)
 		end
 
-		function Meta:DisableMatrix(Str)
+		function Meta:DisableMatrix(Str, ...)
 			if self.IsScalable and self:GetSize() and Str == "RenderMultiply" then
 				local Mat = Matrix()
 
@@ -56,7 +58,7 @@ hook.Add("Initialize", "Scalable Entities", function()
 				return
 			end
 
-			DisMatr(self, Str)
+			DisableMatrix(self, Str, ...)
 		end
 	end
 
