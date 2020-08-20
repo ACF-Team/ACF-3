462511d5c624a0d8bc33ead2dd2d37121f297426
shadow7483147 <tylern64@gmail.com>
2020-08-20 18:11:33 -0500

    Update cl_damage.lua

--- lua/acf/client/cl_damage.lua
+++ lua/acf/client/cl_damage.lua
@@ -1,3 +1,8 @@
+local RandFloat = math.Rand
+local RandInt = math.random
+local Clamp = math.Clamp
+local MathMax = math.max
+
 local Damaged = {
 	CreateMaterial("ACF_Damaged1", "VertexLitGeneric", {
 		["$basetexture"] = "damaged/damaged1"
@@ -68,3 +73,48 @@ net.Receive("ACF_RenderDamage", function()
 	end
 end)
 
+-- Debris & Burning Debris Effects --
+
+local function FadeAway( Ent ) -- local function Entity:FadeAway() is incorrect syntax????????? Am I referencing a hook somehow?
+	if not IsValid(Ent) then return end
+	Ent:SetRenderMode(RENDERMODE_TRANSCOLOR)
+	Ent:SetRenderFX(kRenderFxFadeFast) -- interestingly, not synced to CurTime().
+	timer.Simple(1, function() Ent:Remove() end)
+end
+
+net.Receive("ACF_Debris", function()
+
+	local HitVec = net.ReadVector()
+	local Power = net.ReadFloat()
+	local Mass = net.ReadFloat()
+	local Mdl = net.ReadString()
+	local Mat = net.ReadString()
+	local Col = net.ReadColor()
+	local Pos = net.ReadVector()
+	local Ang = net.ReadAngle()
+
+	local Debris = ents.CreateClientProp(Mdl)
+		Debris:SetPos(Pos)
+		Debris:SetAngles(Ang)
+		Debris:SetColor(Col)
+		if Mat then Debris:SetMaterial(Mat) end
+	Debris:Spawn()
+	local DebrisLifetime = RandFloat(30,60)
+	timer.Simple(DebrisLifetime, function() FadeAway(Debris) end)
+
+	if IsValid(Debris) then
+		local Phys = Debris:GetPhysicsObject()
+		if IsValid(Phys) then
+			Phys:SetMass(Mass*0.1)
+			Phys:ApplyForceOffset(HitVec:GetNormalized() * Power * 70, Debris:GetPos() + VectorRand() * 20)
+		end
+	end
+	-- Trust me when I say I wanted to do so much more than this. Gibs on fire. But very little can be achieved with clientside props...
+	-- some day.
+
+	local BreakEffect = EffectData()
+		BreakEffect:SetOrigin(Pos) -- TODO: Change this to the hit vector, but we need to redefine HitVec as HitNorm
+		BreakEffect:SetScale(20)
+	util.Effect("cball_explode", BreakEffect)
+
+end)
