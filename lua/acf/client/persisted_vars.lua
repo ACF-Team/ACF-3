32d360a7fda67dd84c4c72e7e38de5173d2ea0d6
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2020-12-23 17:54:58 -0300

    Added a few default persisted vars
    - Clients will now persist the values of each crate axis size. Servers will persist a few admin settings.
    - Renamed a few clientside only files.

---
+++ lua/acf/client/persisted_vars.lua
@@ -0,0 +1,5 @@
+-- Variables that should be persisted between servers
+
+ACF.PersistClientData("CrateSizeX", 24)
+ACF.PersistClientData("CrateSizeY", 24)
+ACF.PersistClientData("CrateSizeZ", 24)
---
+++ lua/acf/server/persisted_vars.lua
@@ -0,0 +1,4 @@
+-- Variables that should be persisted between server restarts
+
+ACF.PersistServerData("ServerDataAllowAdmin", false)
+ACF.PersistServerData("ShowFunMenu", true)
