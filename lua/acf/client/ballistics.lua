66f441cf601a7c2b8424e8fd118fad3466a2821c
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2021-10-21 01:51:07 -0300

    Minor optimization to clientside ballistics
    - Localized ACF global table
    - Used Vector:LengthSqr() in favor of elevating the result of Vector:Length() by the power of 2.

--- lua/acf/client/ballistics.lua
+++ lua/acf/client/ballistics.lua
@@ -1,13 +1,15 @@
+local ACF = ACF
+
 ACF.BulletEffect = ACF.BulletEffect or {}
 
 local function BulletFlight(Bullet)
-	local DeltaTime = CurTime() - Bullet.LastThink
-	local Drag = Bullet.SimFlight:GetNormalized() *  (Bullet.DragCoef * Bullet.SimFlight:Length() ^ 2 ) / ACF.DragDiv
+	local DeltaTime  = ACF.CurTime - Bullet.LastThink
+	local Drag       = Bullet.SimFlight:GetNormalized() *  (Bullet.DragCoef * Bullet.SimFlight:LengthSqr()) / ACF.DragDiv
+	local Correction = 0.5 * (Bullet.Accel - Drag) * DeltaTime -- Double integrates constant acceleration for better positional accuracy
 
 	Bullet.SimPosLast = Bullet.SimPos
-	local Correction = 0.5 * (Bullet.Accel - Drag) * DeltaTime   --Double integrates constant acceleration for better positional accuracy
-	Bullet.SimPos = Bullet.SimPos + ACF.Scale * DeltaTime * (Bullet.SimFlight + Correction)  --Calculates the next shell position
-	Bullet.SimFlight = Bullet.SimFlight + (Bullet.Accel - Drag) * DeltaTime			--Calculates the next shell vector
+	Bullet.SimPos     = Bullet.SimPos + ACF.Scale * DeltaTime * (Bullet.SimFlight + Correction) -- Calculates the next shell position
+	Bullet.SimFlight  = Bullet.SimFlight + (Bullet.Accel - Drag) * DeltaTime -- Calculates the next shell vector
 
 	if IsValid(Bullet.Effect) then
 		Bullet.Effect:ApplyMovement(Bullet)
