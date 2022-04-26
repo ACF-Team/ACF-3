1ab4a47b595b8ebaac6f9bfdf3bfbe6c4dfb8a9a
Steve <steejerbs@gmail.com>
2022-04-26 02:16:36 -0500

    New file loader
    - Loads "core" directory first
    - Creates a table under _G.ACF for each directory under the ACF directory e.g. ACF.core, ACF.ballistics, ACF.damage
    - Files use a suffix instead of a prefix to denote realm

--- lua/autorun/acf_loader.lua
+++ lua/autorun/acf_loader.lua
@@ -1,116 +1,99 @@
+local addonName  = "ACF" -- This is the name of the folder that gets loaded
+local globalName = "ACF" -- This is the name of the global table for the addon
 
 --[[
-	Files are sent to client/server/shared realm as determined by folder name (server, client, shared)
-	realm destination inherits through sub-folders
-	Files are sent by one of the following prefixes if not in a realm-specific folder:
-		sv_: server
-		cl_: client
-		sh_: shared (default if no prefix)
-		sk_: skipped/ignored (client only)
+    This script automatically loads all files under the <addonName> folder
+    A global variable is created for the addon named <globalName>
+    The first folders under addonName\ are given tables under globalName, eg. globalName.core, globalName.damage, globalName.fred
 
-	----------------------------------------------------------------------
-	--	IMPORTANT NOTE: file.Find returns files in ALPHABETICAL ORDER	--
-	--		All FOLDERS and FILES are loaded in ALPHABETICAL ORDER		--
-	----------------------------------------------------------------------
+    To reload the addon, run the concmd 'globalName_reload'.
+
+    Files and folders can have their realms specified by adding a suffix to the filename.
+        _cl marks files and folders for CLIENTS
+        _sh marks files and folders for SHARED
+        _sv marks files and folders for SERVERS
+
+    Folder structure:
+
+    addons\
+        addonName\
+            lua\
+                autorun\
+                    loader.lua
+                addonName\
+                    core\
+                        example_sv.lua
+                    damage\
+                        exampleFolder_cl\
+                            thisFileIsSentToClients.lua
+                    ballistics\
 ]]--
-MsgN("\n===========[ Loading ACF ]============\n|")
 
-if not ACF then ACF = {} end
-
-if SERVER then
-	local Realms = {client = "client", server = "server", shared = "shared"}
-	local Text = "| > Loaded %s serverside file(s).\n| > Loaded %s shared file(s).\n| > Loaded %s clientside file(s)."
-	local ServerCount, SharedCount, ClientCount = 0, 0, 0
-
-	local function Load(Path, Realm)
-		local Files, Directories = file.Find(Path .. "/*", "LUA")
-
-		if Realm then -- If a directory specifies which realm then load in that realm and persist through sub-directories
-			for _, File in ipairs(Files) do
-				File = Path .. "/" .. File
-
-				if Realm == "client" then
-					AddCSLuaFile(File)
-
-					ClientCount = ClientCount + 1
-				elseif Realm == "server" then
-					include(File)
-
-					ServerCount = ServerCount + 1
-				else -- Shared
-					include(File)
-					AddCSLuaFile(File)
-
-					SharedCount = SharedCount + 1
-				end
-			end
-		else
-			for _, File in ipairs(Files) do
-				local Sub = string.sub(File, 1, 3)
-
-				File = Path .. "/" .. File
-
-				if Sub == "cl_" then
-					AddCSLuaFile(File)
-
-					ClientCount = ClientCount + 1
-				elseif Sub == "sv_" then
-					include(File)
-
-					ServerCount = ServerCount + 1
-				else -- Shared
-					include(File)
-					AddCSLuaFile(File)
-
-					SharedCount = SharedCount + 1
-				end
-			end
-		end
-
-		for _, Directory in ipairs(Directories) do
-			local Sub = string.sub(Directory, 1, 6)
-
-			Realm = Realms[Sub] or Realm or nil
-
-			Load(Path .. "/" .. Directory, Realm)
-		end
+local realms = {
+    client = "_cl",
+    server = "_sv",
+    shared = "_sh",
+    _sv = include,
+    _cl = AddCSLuaFile,
+    _sh = function(path)
+		include(path)
+		AddCSLuaFile(path)
 	end
+}
 
-	Load("acf")
+setmetatable(realms, realms)
 
-	MsgN(Text:format(ServerCount, SharedCount, ClientCount))
+local function load(path, realm)
+    local files, dirs = file.Find(path .. "/*", "LUA")
 
-elseif CLIENT then
-	local Text = "| > Loaded %s clientside file(s).\n| > Skipped %s clientside file(s)."
-	local FileCount, SkipCount = 0, 0
+    for _, fileName in ipairs(files) do
+        local filePath  = path .. "/" .. fileName
+        local fileSnip  = string.sub(fileName, fileName:len() - 6, fileName:len() - 4)
+        local fileRealm = CLIENT and "_sv" or realms[fileSnip] and fileSnip or realm or "_sh"
 
-	local function Load(Path)
-		local Files, Directories = file.Find(Path .. "/*", "LUA")
+        realms[fileRealm](filePath)
+    end
 
-		for _, File in ipairs(Files) do
-			local Sub = string.sub(File, 1, 3)
+    for _, directory in ipairs(dirs) do
+        local dirSnip  = string.sub(directory, 1, 6)
+        local dirRealm = realms[dirSnip] or realm or "_sh"
 
-			if Sub == "sk_" then
-				SkipCount = SkipCount + 1
-			else
-				File = Path .. "/" .. File
-
-				include(File)
-
-				FileCount = FileCount + 1
-			end
-		end
-
-		for _, Directory in ipairs(Directories) do
-			Load(Path .. "/" .. Directory)
-		end
-	end
-
-	Load("acf")
-
-	MsgN(Text:format(FileCount, SkipCount))
+        load(path .. "/" .. directory, dirRealm)
+    end
 end
 
-MsgN("|\n=======[ Finished Loading ACF ]=======\n")
+local function loadAddon()
+	Msg("\n> " .. addonName .. "/\n")
 
-hook.Run("ACF_OnAddonLoaded")
+    local addonGlobal = {}; _G[addonName] = addonGlobal
+    local  _, dirs    = file.Find(addonName .. "/*", "LUA")
+    local rootFolders = {}
+
+    -- Create a table underneath <addonName> for each folder under the root directory, eg. addonName.core, addonName.damage, addonName.ballistics
+    for _, folderName in ipairs(dirs) do
+        addonGlobal[folderName] = {}
+
+        if folderName == "core" then -- if there is a "core" folder load it first
+            MsgN(" ├──" .. folderName .. "/")
+            load(addonName .. "/" .. folderName)
+        else
+            rootFolders[#rootFolders + 1] = folderName
+        end
+    end
+
+    -- Load all of the root folders except "core" (already loaded)
+    for i, dirName in ipairs(rootFolders) do
+        MsgN((i == #rootFolders and " └──" or  " ├──") .. dirName .. "/")
+
+        local dirSnip  = string.sub(dirName, 1, 6)
+        local dirRealm = realms[dirSnip] or realm or "_sh"
+
+        load(addonName .. "/" .. dirName, dirRealm)
+    end
+end
+
+concommand.Add(string.lower(globalName .. "_reload"), function()
+	loadAddon()
+end)
+
+loadAddon()
