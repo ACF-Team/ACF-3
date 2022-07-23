c9642543c89509f792482ccd78b4e4ac6d578062
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2022-07-23 19:39:00 -0400

    Cleaned up playerstart chairs file

--- lua/acf/entities/playerstart_chairs.lua
+++ lua/acf/entities/playerstart_chairs.lua
@@ -1,13 +1,24 @@
-local function AddVehicle( t, class )
-	list.Set( "Vehicles", class, t )
-end
+local Category = "Playerstart Chairs"
 
-local lCategory = "Playerstart Chairs"
-local lAuthor = "aversion"
-local lInformation = "Driving pose modeled with a playermodel mesh."
-local lClass = "prop_vehicle_prisoner_pod"
-local lKeyvalues = { vehiclescript = "scripts/vehicles/prisoner_pod.txt", limitview = "0" }
-local lCategoryExp = "Playerstart Chairs (Experimental)"
+local function AddVehicle(Class, Data)
+	local Entry = {
+		Name        = Data.Name,
+		Model       = Data.Model,
+		Class       = "prop_vehicle_prisoner_pod",
+		Category    = Category,
+		Author      = "aversion",
+		Information = "Driving pose modeled with a playermodel mesh.",
+		KeyValues   = {
+			vehiclescript = "scripts/vehicles/prisoner_pod.txt",
+			limitview     = "0"
+		},
+		Members = {
+			HandleAnimation = Data.Animation,
+		},
+	}
+
+	list.Set("Vehicles", Class, Entry)
+end
 
 local function HandleRollercoasterAnimation(_, Player)
 	return Player:SelectWeightedSequence(ACT_GMOD_SIT_ROLLERCOASTER)
@@ -25,111 +36,49 @@ local function HandlePodAnim(_, Player)
 	return Player:LookupSequence("drive_pd")
 end
 
-AddVehicle( {
-	Name = "Jeep Pose",
-	Model = "models/chairs_playerstart/jeeppose.mdl",
-	Class = lClass,
-	Category = lCategory,
+AddVehicle("playerstart_chairs_jeep", {
+	Name      = "Jeep Pose",
+	Model     = "models/chairs_playerstart/jeeppose.mdl",
+	Animation = HandlePHXVehicleAnimation,
+})
 
-	Author = lAuthor,
-	Information = lInformation,
+AddVehicle("playerstart_chairs_airboat", {
+	Name      = "Airboat Pose",
+	Model     = "models/chairs_playerstart/airboatpose.mdl",
+	Animation = HandlePHXAirboatAnimation,
+})
 
-	KeyValues = lKeyvalues,
-	Members = {
-		HandleAnimation = HandlePHXVehicleAnimation, ----
-	}
-}, "playerstart_chairs_jeep" )
+AddVehicle("playerstart_chairs_seated", {
+	Name      = "Sitting Pose",
+	Model     = "models/chairs_playerstart/sitposealt.mdl",
+	Animation = HandleRollercoasterAnimation,
+})
 
-AddVehicle( {
-	Name = "Airboat Pose",  ----
-	Model = "models/chairs_playerstart/airboatpose.mdl", ----
-	Class = lClass,
-	Category = lCategory,
-
-	Author = lAuthor,
-	Information = lInformation,
-
-	KeyValues = lKeyvalues,
-	Members = {
-		HandleAnimation = HandlePHXAirboatAnimation, ----
-	}
-}, "playerstart_chairs_airboat" )
-
-AddVehicle( {
-	Name = "Sitting Pose",  ----
-	Model = "models/chairs_playerstart/sitposealt.mdl", ----
-	Class = lClass,
-	Category = lCategory,
-
-	Author = lAuthor,
-	Information = lInformation,
-
-	KeyValues = lKeyvalues,
-	Members = {
-		HandleAnimation = HandleRollercoasterAnimation, ----
-	}
-}, "playerstart_chairs_seated" )
-
-AddVehicle( {
-	Name = "Pod Pose",  ----
-	Model = "models/chairs_playerstart/podpose.mdl", ----
-	Class = lClass,
-	Category = lCategory,
-
-	Author = lAuthor,
-	Information = lInformation,
-
-	KeyValues = lKeyvalues,
-	Members = {
-		HandleAnimation = HandlePodAnim, ----
-	}
-}, "playerstart_chairs_podpose" )
+AddVehicle("playerstart_chairs_podpose", {
+	Name      = "Pod Pose",
+	Model     = "models/chairs_playerstart/podpose.mdl",
+	Animation = HandlePodAnim,
+})
 
 
 ---- Experimental seats ----
+Category = "Playerstart Chairs (Experimental)"
 
 
-AddVehicle( {
-	Name = "Sitting Pose (Alt Physics)",  ----
-	Model = "models/chairs_playerstart/sitpose.mdl", ----
-	Class = lClass,
-	Category = lCategoryExp,
+AddVehicle("playerstart_chairs_seated_alt", {
+	Name      = "Sitting Pose (Alt Physics)",
+	Model     = "models/chairs_playerstart/sitpose.mdl",
+	Animation = HandleRollercoasterAnimation,
+})
 
-	Author = lAuthor,
-	Information = lInformation,
+AddVehicle("playerstart_chairs_standing", {
+	Name      = "Standing Pose",
+	Model     = "models/chairs_playerstart/standingpose.mdl",
+	Animation = HandlePodAnim,
+})
 
-	KeyValues = lKeyvalues,
-	Members = {
-		HandleAnimation = HandleRollercoasterAnimation, ----
-	}
-}, "playerstart_chairs_seated_alt" )
-
-AddVehicle( {
-	Name = "Standing Pose",  ----
-	Model = "models/chairs_playerstart/standingpose.mdl", ----
-	Class = lClass,
-	Category = lCategoryExp,
-
-	Author = lAuthor,
-	Information = lInformation,
-
-	KeyValues = lKeyvalues,
-	Members = {
-		HandleAnimation = HandlePodAnim, ----
-	}
-}, "playerstart_chairs_standing" )
-
-AddVehicle( {
-	Name = "Prone Pose",  ----
-	Model = "models/chairs_playerstart/pronepose.mdl", ----
-	Class = lClass,
-	Category = lCategoryExp,
-
-	Author = lAuthor,
-	Information = lInformation,
-
-	KeyValues = lKeyvalues,
-	Members = {
-		HandleAnimation = HandlePodAnim, ----
-	}
-}, "playerstart_chairs_prone" )
+AddVehicle("playerstart_chairs_prone", {
+	Name      = "Prone Pose",
+	Model     = "models/chairs_playerstart/pronepose.mdl",
+	Animation = HandlePodAnim,
+})
