5908b3dcf79499995317321ad0644fdc76c3bebc
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-08-10 19:46:59 -0700

    Add KEShove tests

---
+++ lua/tests/acf/core/damage/damage_sv/acf_keshove.lua
@@ -0,0 +1,124 @@
+local ones = Vector( 1, 1, 1 )
+
+return {
+    groupName = "ACF.KeShove",
+
+    beforeEach = function( state )
+        state.applyForceStub = stub()
+
+        state.ent = {
+            acftotal = 1,
+            acfphystotal = 1,
+            acflastupdatemass = math.huge,
+
+            IsValid = function() return true end,
+            WorldToLocal = function() return ones end,
+            LocalToWorld = function() return ones end,
+            GetPhysicsObject = function()
+                return {
+                    IsValid = function()
+                        return true
+                    end,
+
+                    ApplyForceOffset = state.applyForceStub
+                }
+            end,
+        }
+
+        -- For simplicity's sake, we'll pretend the ent's ancestor is itself
+        stub( _G, "ACF_GetAncestor" ).returns( state.ent )
+    end,
+
+    cases = {
+        {
+            name = "Shoves the target",
+            func = function( state )
+                local ent = state.ent
+
+                ACF.KEShove( ent, ones, ones, 1 )
+
+                expect( state.applyForceStub ).to.haveBeenCalled()
+            end
+        },
+
+        {
+            name = "Does not shove invalid targets",
+            func = function( state )
+                local ent = state.ent
+                ent.IsValid = function() return false end
+
+                ACF.KEShove( ent, ones, ones, 1 )
+
+                expect( state.applyForceStub ).toNot.haveBeenCalled()
+            end
+        },
+        {
+            name = "Does not shove the entity if ACF_KEShove hook returns false",
+            func = function( state )
+                hook.Add( "ACF_KEShove", "Test", function() return false end )
+                local ent = state.ent
+
+                ACF.KEShove( ent, ones, ones, 1 )
+
+                expect( state.applyForceStub ).toNot.haveBeenCalled()
+            end,
+
+            cleanup = function()
+                hook.Remove( "ACF_KEShove", "Test" )
+            end
+        },
+
+        {
+            name = "Calculates the Mass if acflastupdatemass is absent",
+            func = function( state )
+                local ent = state.ent
+                ent.acflastupdatemass = nil
+
+                local calcMass = stub( _G, "ACF_CalcMassRatio" )
+                ACF.KEShove( ent, ones, ones, 1 )
+
+                expect( calcMass ).to.haveBeenCalled()
+            end
+        },
+
+        {
+            name = "Calculates the Mass if acflastupdatemass is stale",
+            func = function( state )
+                local ent = state.ent
+                ent.acflastupdatemass = -math.huge
+
+                local calcMass = stub( _G, "ACF_CalcMassRatio" )
+                ACF.KEShove( ent, ones, ones, 1 )
+
+                expect( calcMass ).to.haveBeenCalled()
+            end
+        },
+
+        {
+            name = "Async test",
+            async = true,
+            timeout = 2,
+            func = function()
+                local og = function() return "test1" end
+
+                local var = {
+                    func = og
+                }
+
+                local funcStub = stub( var, "func" ).returns( "test2" )
+
+                timer.Simple( 1, function()
+                    expect( var.func() ).to.equal( "test2" )
+                    expect( funcStub ).to.haveBeenCalled()
+
+                    funcStub:Restore()
+                end )
+
+                timer.Simple( 1.5, function()
+                    expect( var.func ).to.equal( og )
+                    done()
+                end )
+            end
+        }
+    }
+}
