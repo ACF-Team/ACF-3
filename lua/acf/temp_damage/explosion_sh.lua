9d79fd7db136e102109dad3b2205efe292ea53a4
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2022-11-06 14:32:07 -0300

    Added explosionEffect function
    - Added shared ACF.TempDamage.explosionEffect helper function. It covers everything you need to create an ACF explosion effect.

--- lua/acf/temp_damage/explosion_sh.lua
+++ lua/acf/temp_damage/explosion_sh.lua
@@ -1,5 +1,8 @@
-local ACF = ACF
+local math   = math
+local util   = util
+local ACF    = ACF
 local Damage = ACF.TempDamage
+local Down   = Vector(0, 0, -1)
 
 --- Returns the blast radius based on a given amount of filler mass
 -- Note: Scaling law found on the net, based on 1PSI overpressure from 1 kg of TNT at 15m
@@ -7,4 +10,16 @@ local Damage = ACF.TempDamage
 -- @return The blast radius in inches.
 function Damage.getBlastRadius(Filler)
 	return Filler ^ 0.33 * 8 * 39.37
+end
+
+function Damage.explosionEffect(Position, Direction, Filler, Caliber)
+	local Radius = math.max(1, Damage.getBlastRadius(Filler))
+
+	local Effect = EffectData()
+	Effect:SetOrigin(Position)
+	Effect:SetNormal(Direction or Down)
+	Effect:SetRadius(Caliber or 0)
+	Effect:SetScale(Radius)
+
+	util.Effect("ACF_Explosion", Effect)
 end
