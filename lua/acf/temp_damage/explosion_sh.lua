592a67ab3732b602bc30166d8ddc7eef312813ea
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2022-11-06 14:42:23 -0300

    Documented explosionEffect

--- lua/acf/temp_damage/explosion_sh.lua
+++ lua/acf/temp_damage/explosion_sh.lua
@@ -12,6 +12,13 @@ function Damage.getBlastRadius(Filler)
 	return Filler ^ 0.33 * 8 * 39.37
 end
 
+--- Helper function to create the default ACF explosion effect.
+-- @param Position The world position at which the effect will be created.
+-- @param Direction A vector referencing the direction at which the explosion will move towards.
+-- Leave this blank to create an upwards explosion effect.
+-- @param Filler The filler mass of the explosive in kilograms.
+-- @param Caliber The diameter in milimeters of the projectile that created this effect.
+-- You can leave this blank if no projectile was involved on the creation of this effect.
 function Damage.explosionEffect(Position, Direction, Filler, Caliber)
 	local Radius = math.max(1, Damage.getBlastRadius(Filler))
 
