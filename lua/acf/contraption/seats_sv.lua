92eccce8e911465a21eacb6c7b7f63576d2e297e
march <106459595+marchc1@users.noreply.github.com>
2024-03-06 00:48:54 -0800

    Add seat model validation check

--- lua/acf/contraption/seats_sv.lua
+++ lua/acf/contraption/seats_sv.lua
@@ -1,5 +1,36 @@
 -- Optional functionality for legality checking on all vehicles
 
+-- A whitelist of allowed seats, prevents setting your seat to something with a microscopic physics object
+local ValidSeatModels = {
+    -- Default HL2 seats
+	["models/nova/airboat_seat.mdl"] = true,
+	["models/nova/chair_office02.mdl"] = true,
+	["models/props_phx/carseat2.mdl"] = true,
+	["models/props_phx/carseat2.mdl"] = true,
+	["models/props_phx/carseat3.mdl"] = true,
+	["models/props_phx/carseat2.mdl"] = true,
+	["models/nova/chair_plastic01.mdl"] = true,
+	["models/nova/jeep_seat.mdl"] = true,
+	["models/nova/chair_office01.mdl"] = true,
+	["models/nova/chair_wood01.mdl"] = true,
+	["models/vehicles/prisoner_pod_inner.mdl"] = true,
+
+    -- ACF default seats
+	["models/vehicles/driver_pod.mdl"] = true,
+	["models/vehicles/pilot_seat.mdl"] = true,
+
+	["models/chairs_playerstart/airboatpose.mdl"] = true,
+	["models/chairs_playerstart/jeeppose.mdl"] = true,
+	["models/chairs_playerstart/podpose.mdl"] = true,
+	["models/chairs_playerstart/sitposealt.mdl"] = true,
+	["models/chairs_playerstart/pronepose.mdl"] = true,
+	["models/chairs_playerstart/sitpose.mdl"] = true,
+	["models/chairs_playerstart/standingpose.mdl"] = true,
+	
+	["models/lubprops/seat/raceseat.mdl"] = true,
+	["models/lubprops/seat/raceseat2.mdl"] = true,
+}
+
 hook.Add("OnEntityCreated", "ACF_SeatLegality", function(Entity)
     timer.Simple(0, function()
         if not IsValid(Entity) then return end
@@ -32,6 +63,12 @@ hook.Add("OnEntityCreated", "ACF_SeatLegality", function(Entity)
 
         ACF.CheckLegal(Entity)
     end)
+end)
+
+hook.Add("ACF_IsLegal", "ACF_CheckLegal_SeatLegality", function(Entity)
+    if ACF.VehicleLegalChecks and Entity:IsVehicle() and not ValidSeatModels[Entity:GetModel()] then 
+        return false, "Bad Seat Model", "This seat model is not on the whitelist." 
+    end
 end)
 
 hook.Add("CanPlayerEnterVehicle", "ACF_SeatLegality", function(Player, Entity)
