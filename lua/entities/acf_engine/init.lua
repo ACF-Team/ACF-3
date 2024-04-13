9709a582ac96070b2ad46dabe7049818f51fddfd
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2024-04-13 04:23:32 -0400

    Added missing line for current engine sound path
    - Added a missile line of code that references the current sound path on an engine when its sounds get updated, fixes #400
    - Edited a comment that was messing with the syntax highlighing on Github.

--- lua/entities/acf_engine/init.lua
+++ lua/entities/acf_engine/init.lua
@@ -127,7 +127,7 @@ local function GetPitchVolume(Engine)
 	local RPM = Engine.FlyRPM
 	local Pitch = Clamp(20 + (RPM * Engine.SoundPitch) * 0.02, 1, 255)
 	-- Rev limiter code disabled because it has issues with the volume delta time, but it's still here if we need it
-	local Throttle = --[[Engine.RevLimited and 0 or]] Engine.Throttle
+	local Throttle = Engine.Throttle -- Engine.RevLimited and 0 or Engine.Throttle
 	local Volume = 0.25 + (0.1 + 0.9 * ((RPM / Engine.LimitRPM) ^ 1.5)) * Throttle * 0.666
 
 	return Pitch, Volume * Engine.SoundVolume
@@ -611,6 +611,8 @@ end
 
 function ENT:UpdateSound(SelfTbl)
 	SelfTbl = SelfTbl or self:GetTable()
+
+	local Path      = SelfTbl.SoundPath
 	local LastSound = SelfTbl.LastSound
 
 	if Path ~= LastSound and LastSound ~= nil then
