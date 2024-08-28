a87e9c7c3d3118490325b57a5aaf8dd2b96e3ba8
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2024-08-27 20:05:19 -0400

    Add warning to install CFW if not found

--- lua/acf/compatibility/conflict_checker.lua
+++ lua/acf/compatibility/conflict_checker.lua
@@ -1,3 +1,4 @@
+local Messages = ACF.Utilities.Messages
 
 -- Older versions of ACF will conflict due to the new file loading system we implemented.
 -- If the server has an older version installed simultaneously, we'll let the players know.
@@ -14,7 +15,7 @@ if ACF.Version then
         net.Receive("ACF_VersionConflict", function()
             hook.Add("CreateMove", "ACF Version Conflict", function(Move)
                 if Move:GetButtons() ~= 0 then
-                    ACF.PrintToChat("Warning", "An unsupported or multiple versions of ACF detected. Check server's ACF installation.")
+                    Messages.PrintChat("Warning", "An unsupported or multiple versions of ACF detected. Check server's ACF installation.")
 
                     hook.Remove("CreateMove", "ACF Version Conflict")
                 end
@@ -22,3 +23,18 @@ if ACF.Version then
         end)
     end
 end
+
+-- Make sure that players also know they need to install CFW if they haven't already
+
+timer.Simple(1, function()
+    if not CFW then
+        hook.Add("CreateMove", "ACF CFW Requirement", function(Move)
+            if Move:GetButtons() ~= 0 then
+                local PrintFunc = CLIENT and Messages.PrintChat or Messages.PrintLog
+                PrintFunc("Warning", "Contraption Framework is not installed! ACF will not work correctly! Install it at https://steamcommunity.com/sharedfiles/filedetails/?id=3154971187")
+
+                hook.Remove("CreateMove", "ACF Version Conflict")
+            end
+        end)
+    end
+end)
