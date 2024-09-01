97d1c2ca7dbc13fb8feb602bb0e302cda4a39d5b
march <106459595+marchc1@users.noreply.github.com>
2024-09-01 11:39:25 -0700

    Localize variables

--- lua/entities/acf_baseplate/cl_init.lua
+++ lua/entities/acf_baseplate/cl_init.lua
@@ -4,31 +4,40 @@ function ENT:Update() end
 
 local HideInfo = ACF.HideInfoBubble
 
+local COLOR_Black = Color(0, 0, 0)
+local COLOR_Red   = Color(255, 50, 40)
+local COLOR_Green = Color(40, 255, 50)
+
 function ENT:DrawGizmos()
     cam.IgnoreZ(true)
 
-    render.DrawBeam(self:GetPos(), self:LocalToWorld(Vector(self.Size.x / 2, 0, 0)), 2, 0, 1, Color(0, 0, 0))
-    render.DrawBeam(self:GetPos(), self:LocalToWorld(Vector(self.Size.x / 2, 0, 0)), 1, 0, 1, Color(255, 50, 40))
-    render.DrawBeam(self:GetPos(), self:LocalToWorld(Vector(0, -self.Size.y / 2, 0)), 2, 0, 1, Color(0, 0, 0))
-    render.DrawBeam(self:GetPos(), self:LocalToWorld(Vector(0, -self.Size.y / 2, 0)), 1, 0, 1, Color(40, 255, 50))
+    local Pos = self:GetPos()
+    local Size = self.Size
+
+    render.DrawBeam(Pos, self:LocalToWorld(Vector(Size.x / 2, 0, 0)), 2, 0, 1, COLOR_Black)
+    render.DrawBeam(Pos, self:LocalToWorld(Vector(Size.x / 2, 0, 0)), 1, 0, 1, COLOR_Red)
+    render.DrawBeam(Pos, self:LocalToWorld(Vector(0, -Size.y / 2, 0)), 2, 0, 1, COLOR_Black)
+    render.DrawBeam(Pos, self:LocalToWorld(Vector(0, -Size.y / 2, 0)), 1, 0, 1, COLOR_Green)
 
     cam.IgnoreZ(false)
 end
 
 function ENT:Draw()
     -- Partial from base_wire_entity, need the tooltip but without the model drawing since we're drawing our own
-    local looked_at = self:BeingLookedAtByLocalPlayer()
+    local LocalPlayer = LocalPlayer()
+    local Weapon      = LocalPlayer:GetActiveWeapon()
+    local LookedAt    = self:BeingLookedAtByLocalPlayer()
 
-    if looked_at then
+    if LookedAt then
         self:DrawEntityOutline()
     end
 
     self:DrawModel()
 
-    if looked_at and not HideInfo() then
+    if LookedAt and not HideInfo() then
         self:AddWorldTip()
 
-        if not LocalPlayer():InVehicle() and IsValid(LocalPlayer():GetActiveWeapon()) and LocalPlayer():GetActiveWeapon():GetClass() == "weapon_physgun" then
+        if not LocalPlayer:InVehicle() and IsValid(Weapon) and Weapon:GetClass() == "weapon_physgun" then
             self:DrawGizmos()
         end
     end
