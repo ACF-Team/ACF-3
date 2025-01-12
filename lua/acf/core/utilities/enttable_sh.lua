910b64cb5010109f306c4c7086a853f420ee3c9b
march <106459595+marchc1@users.noreply.github.com>
2025-01-12 15:57:04 -0800

    Potentially 150x faster indexing
    Tested with x86-64 JIT, in general the more we can avoid __index like the plague the better

---
+++ lua/acf/core/utilities/enttable_sh.lua
@@ -0,0 +1,61 @@
+local EntTable = {
+    Entity2Table = {}
+}
+
+ACF.EntityTable = EntTable
+
+local ent2tbl = EntTable.Entity2Table
+
+function EntTable.Get(ent, key)
+    local tbl = ent2tbl[ent]
+
+    if not tbl then
+        tbl = ent:GetTable()
+        ent2tbl[ent] = tbl
+    end
+
+    return tbl[key]
+end
+
+function EntTable.Set(ent, key, value)
+    local tbl = ent2tbl[ent]
+
+    if not tbl then
+        tbl = ent:GetTable()
+        ent2tbl[ent] = tbl
+    end
+
+    tbl[key] = value
+end
+
+hook.Add("EntityRemoved", "ACF.EntityTable.EntityRemoved", function(ent)
+    ent2tbl[ent] = nil
+end)
+
+--[[
+    local ent = Entity(90)
+
+    local st = SysTime
+    local startTime, endTime
+    print("")
+    print("Profiling Results:")
+    print("")
+    
+    local get, set = EntTable.Get, EntTable.Set
+    
+    startTime = st()
+    for _ = 1, 100000 do
+        ent.Test = 5
+        -- set(ent, "Test", 5)
+    end
+    endTime = st()
+    print("Variables.Set: " .. ((endTime - startTime) * 1000) .. " ms")
+    
+    startTime = st()
+    for _ = 1, 100000 do
+        local t = ent.Test
+        -- get(ent, "Test")
+    end
+    endTime = st()
+    print("Variables.Get: " .. ((endTime - startTime) * 1000) .. " ms")
+]]
