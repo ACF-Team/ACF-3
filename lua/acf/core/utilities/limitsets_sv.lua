8c5025382250e02d1fa401e984b25d581cc6a610
march <106459595+marchc1@users.noreply.github.com>
2025-01-16 18:07:47 -0800

    Add LimitSets library

---
+++ lua/acf/core/utilities/limitsets_sv.lua
@@ -0,0 +1,42 @@
+local LimitSets = {
+    Registered = {}
+}
+ACF.LimitSets = LimitSets
+
+function LimitSets.Create(Name)
+    local Object = {
+        ServerData = {}
+    }
+
+    LimitSets.Registered[Name] = Object
+
+    function Object:SetServerData(Key, Value)
+        self.ServerData[Key] = Value
+    end
+
+    return Object
+end
+
+function LimitSets.Execute(Name)
+    ACF.Utilities.Messages.PrintLog("Info", "Loading the limitset '" .. Name .. "'...")
+
+    if not isstring(Name) then return false, "Argument #1 (Name) must be a string." end
+
+    local LimitSet = LimitSets.Registered[Name]
+    if not LimitSet then return false, "No limitset with the name '" .. Name .. "'." end
+
+    for Key, Value in pairs(LimitSet.ServerData) do
+        ACF.SetServerData(Key, Value)
+    end
+end
+
+hook.Add("ACF_OnLoadAddon", "ACF_LimitSets_Setup", function()
+    local SelectedLimitsetName = ACF.SelectedLimitset or "PvP"
+    local SelectedLimitset     = ACF.LimitSets.Registered[SelectedLimitsetName]
+
+    if SelectedLimitset then
+        LimitSets.Execute(SelectedLimitsetName)
+    else
+        ACF.Utilities.Messages.PrintLog("Info", "No limitset loaded.")
+    end
+end)
