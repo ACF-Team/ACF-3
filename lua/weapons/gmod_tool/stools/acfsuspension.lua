e64ea3bffdba7ecc6b8771bb2ea12c4e35ef5fb5
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2025-05-13 02:49:12 -0400

    Finish Suspension Tool

--- lua/weapons/gmod_tool/stools/acfsuspension.lua
+++ lua/weapons/gmod_tool/stools/acfsuspension.lua
@@ -1,18 +1,10 @@
 --[[
-This is NOT clean code... It should be optimized enough though. 
-I tried piecing this together as fast as possible due to time constraints.
-If someone wants to improve it they are welcome to do so.
-
-Due to the specific keybinds i need, doing it through the ACF tool functions is not possible.
-I did use ACF's menu building/reload functions for rapid development though.
-
-The basic controller functions were modified from Prop To Mesh ()
+Notes:
+- We treat baseplates as the first plate and the rest as steer plates.
+- Since we have a mapping from plates to wheels, we also know which wheels belong to the baseplate, or any of the given steer plates.
 --]]
 
 local ACF = ACF
-local Contraption	= ACF.Contraption
-local Entities   = ACF.Classes.Entities
-local Messages   = ACF.Utilities.Messages
 local IsValid = IsValid
 
 TOOL.Category	 = (ACF.CustomToolCategory and ACF.CustomToolCategory:GetBool()) and "ACF" or "Construction"
@@ -20,67 +12,35 @@ TOOL.Name		 = "ACF Suspension Tool"
 TOOL.Command	 = nil
 TOOL.ConfigName	 = ""
 TOOL.Information = {
-	{ name = "right0shift", stage = 0, desc = "[SHIFT] Select a baseplate", icon2 = "gui/key.png" },
-	{ name = "right1", stage = 1, desc = "Select a left drive wheel" },
-	{ name = "right2", stage = 2, desc = "Select a right drive wheel" },
-	{ name = "right3", stage = 3, desc = "Select a left idler wheel" },
-	{ name = "right4", stage = 4, desc = "Select a right idler wheel" },
-	{ name = "right5", stage = 5, desc = "Select a road wheel" },
-	{ name = "right5shift", stage = 5, desc = "[SHIFT] Select a steer plate" },
-	{ name = "right6", stage = 6, desc = "Select a steer plate" },
-	{ name = "right6shift", stage = 6, desc = "[SHIFT] Select a steer wheel" },
-	{ name = "reload", desc = "Reset the tool", icon2 = "gui/refresh.png", },
+	-- Note: desc is not a valid property, I just use it to generate language descriptions. (See language.Add)
+	{name = "right0shift", stage = 0, desc = "[SHIFT] Select a baseplate", icon2 = "gui/key.png"},
+	{name = "right1", stage = 1, desc = "Select a wheel"},
+	{name = "right1shift", stage = 1, desc = "[SHIFT] Select a steer plate", icon2 = "gui/key.png"},
+	{name = "reload", desc = "Reset the tool", icon2 = "gui/refresh.png"}
 }
 
 -- Stage 0 SHIFT + RMB: Baseplate -> Stage 1
--- Stage 1 RMB: Left Drive Wheel -> Stage 2
--- Stage 2 RMB: Right Drive Wheel -> Stage 3
--- Stage 3 RMB: Left Idler Wheel -> Stage 4
--- Stage 4 RMB: Right Idler Wheel -> Stage 5
--- Stage 5 RMB: Road wheels -> Stage 5
--- Stage 5 SHIFT + RMB: Steer Plate -> Stage 6
--- Stage 6 RMB: Steer wheel -> Stage 6
--- Stage 6 SHIFT + RMB: Steer Plate -> Stage 6
+-- Stage 1 RMB: Road wheels, ordered left/right starting from drive wheels and idlers
+-- Stage 1 SHIFT + RMB: Steer plate -> Stage 1
 
-TOOL.ColMatMap = {}
-TOOL.Selections = {}
-
-TOOL.Baseplate = nil
-TOOL.DriveWheels = {}
-TOOL.IdlerWheels = {}
-TOOL.RoadWheels = {}
-TOOL.SteerPlates = {}
-TOOL.SteerWheels = {}
-TOOL.SteerPlatesToWheels = {}
-
-function TOOL:SelectColor(Entity, Col, Material)
-	self.ColMatMap[Entity] = {Entity:GetColor(), Entity:GetMaterial()}
-	Entity:SetColor(Col or Color(0, 255, 0, 255))
-	Entity:SetMaterial(Material or "models/wireframe")
-end
-
-function TOOL:UnselectColor(Entity)
-	if self.ColMatMap[Entity] then
-		Entity:SetColor(self.ColMatMap[Entity][1])
-		Entity:SetMaterial(self.ColMatMap[Entity][2])
-		self.ColMatMap[Entity] = nil
-	end
-end
-
+TOOL.Selections = {}				-- Holds the selections
+TOOL.Selections.Plates = {}			-- Holds the plates
+TOOL.Selections.Wheels = {}			-- Holds the wheels
+TOOL.Selections.PlatesToWheels = {}	-- Holds the wheels for each plate
 
 if CLIENT then
-	language.Add("tool.acfsuspension.name", "ACF Armor Compiler")
-	language.Add("tool.acfsuspension.desc", "Aids in editting an ACF Armor Mesh")
-
-	surface.CreateFont("Torchfont", { size = 40, weight = 1000, font = "arial" })
+	-- Add descriptions to tool info
+	language.Add("tool.acfsuspension.name", "ACF Suspension Tool")
+	language.Add("tool.acfsuspension.desc", "Aids in setting up basic suspensions.")
 
 	for _, v in pairs(TOOL.Information) do
 		language.Add("tool.acfsuspension." .. v.name, v.desc)
 	end
 
-	--- Making the menu actually work...
+	CreateClientConVar("ACF_Sus_Tool_IsTracked", 1, false, true)
+	CreateClientConVar("ACF_Sus_Tool_Use_Custom", 0, false, true)
 
-	--- Creates (Or recreates) the menu for this tool
+	--- Creates/recreates the menu for this tool
 	local function CreateMenu(Panel)
 		local Menu = ACF.ArmorMenu
 
@@ -97,50 +57,113 @@ if CLIENT then
 		end
 
 		local Reload = Menu:AddButton("Reload Menu")
-		Reload:SetTooltip("You can also type 'acf_reload_armor_menu' in console.")
+		Reload:SetTooltip("You can also type 'acf_reload_suspension_menu' in console.")
 		function Reload:DoClickInternal()
-			RunConsoleCommand("acf_reload_armor_menu")
+			RunConsoleCommand("acf_reload_suspension_menu")
 		end
 
-		Menu:AddLabel("ACF Armor Mesh Tool")
-		Menu:AddLabel("This tool is used to create and edit ACF armor meshes.")
+		Menu:AddTitle("ACF Suspension Tool")
+		Menu:AddLabel("This tool helps create constraints for basic drivetrains.")
+
+		local IsTracked = Menu:AddCheckBox("Drivetrain Uses Tracks", "ACF_Sus_Tool_IsTracked")
+		IsTracked:SetTooltip("If checked, the drivetrain will be tracked. Otherwise, it will be wheeled.")
+
+		local UseCustom = Menu:AddCheckBox("Use my own suspension", "ACF_Sus_Tool_Use_Custom")
+		UseCustom:SetTooltip("If checked, the drivetrain will only make your wheels rotate propperly.\nYou will need to create the constraints that hold it in place/suspend it yourself.")
+
+		local Create = Menu:AddButton("Create Drivetrain")
+		Create:SetTooltip("Creates a new drivetrain with the selected entitites.")
+
+		function Create:DoClickInternal()
+			net.Start("ACF_Sus_Tool_Server")
+			net.WriteString("Create")
+			net.WriteUInt(LocalPlayer():EntIndex(), 16)
+			net.SendToServer()
+		end
+
+		local Clear = Menu:AddButton("Clear Drivetrain")
+		Clear:SetTooltip("Clears all constraints on selected entities.")
+
+		function Clear:DoClickInternal()
+			net.Start("ACF_Sus_Tool_Server")
+			net.WriteString("Clear")
+			net.WriteUInt(LocalPlayer():EntIndex(), 16)
+			net.SendToServer()
+		end
+
+		Menu:AddTitle("Instructions (General)")
+		Menu:AddLabel("When selecting wheels, select in alternating order starting with left then right.")
+		Menu:AddLabel("If you hold the tool, the entities you selected will be labelled, which should help.")
+		Menu:AddLabel("Pressing R at any time will reset the tool's record of the selected entities, in case you made a mistake.")
+
+		Menu:AddTitle("Instructions (Tracked)")
+		Menu:AddLabel("1. Select the baseplate with SHIFT + RMB")
+		Menu:AddLabel("2. Select left/right drive wheels with RMB")
+		Menu:AddLabel("3. Select left/right road wheels with RMB")
+		Menu:AddLabel("4. If you ever want to add a steer plate, select it with SHIFT + RMB")
+		Menu:AddLabel("5. Then all steer wheels selected after will belong to that steer plate")
+
+		Menu:AddTitle("Instructions (Wheeled)")
+		Menu:AddLabel("1. Select the baseplate with SHIFT + RMB")
+		Menu:AddLabel("2. Select left/right road wheels with RMB")
+		Menu:AddLabel("3. If you ever want to add a steer plate, select it with SHIFT + RMB")
+		Menu:AddLabel("4. Then all steer wheels selected after will belong to that steer plate")
 	end
 
 	TOOL.BuildCPanel = CreateMenu
 
-	concommand.Add("acf_reload_armor_menu", function()
+	concommand.Add("acf_reload_suspension_menu", function()
 		if not IsValid(ACF.ArmorMenu) then return end
 
 		CreateMenu(ACF.ArmorMenu.Panel)
 	end)
 
-	local TextGray = Color(224, 224, 255)
-	local BGGray = Color(200, 200, 200)
-	local Blue = Color(50, 200, 200)
-	local Red = Color(200, 50, 50)
-	local Green = Color(50, 200, 50)
-	local Black = Color(0, 0, 0)
-	local drawText = draw.SimpleTextOutlined
+	net.Receive("ACF_Sus_Tool", function()
+		local Player = LocalPlayer()
+		Player.ACF_Sus_Tool_Info = net.ReadTable()
+	end)
 
-	surface.CreateFont("ACF_ToolTitle", {
-		font = "Arial",
-		size = 32
-	})
+	function DrawEntText(Entity, Text, Col)
+		local Pos = Entity:GetPos()
+		local SP = Pos:ToScreen()
+		draw.SimpleTextOutlined(Text, nil, SP.x, SP.y, Col, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0, 0, 0))
+	end
 
-	surface.CreateFont("ACF_ToolSub", {
-		font = "Arial",
-		size = 25
-	})
+	function DrawEntLink(Entity1, Entity2, Col)
+		local Pos1 = Entity1:GetPos()
+		local SP1 = Pos1:ToScreen()
+		local Pos2 = Entity2:GetPos()
+		local SP2 = Pos2:ToScreen()
 
-	surface.CreateFont("ACF_ToolLabel", {
-		font = "Arial",
-		size = 32,
-		weight = 620,
-	})
+		surface.SetDrawColor(Col)
+		surface.DrawLine(SP1.x, SP1.y, SP2.x, SP2.y)
+	end
 
 	--- Draws the hud/tooltips for this tool
 	function TOOL:DrawHUD()
+		local Player = LocalPlayer()
+		local Selections = Player.ACF_Sus_Tool_Info or {}
+		local IsTracked = GetConVar("ACF_Sus_Tool_IsTracked"):GetInt()
 
+		-- For each plate...
+		for k, v in ipairs(Selections.Plates or {}) do
+			if not IsValid(v) then continue end
+			if k == 1 then DrawEntText(v, "Baseplate", Color(255, 0, 255))
+			else DrawEntText(v, "Steer plate", Color(0, 255, 255)) end
+
+			-- For each wheel of the plate...
+			for k2, w in ipairs(Selections.PlatesToWheels[v] or {}) do
+				if not IsValid(w) then continue end
+				local Direction = IsTracked and k2 % 2 == 1 and " Left" or " Right"
+				local Steer = k > 1 and " Steer" or ""
+				local Drive = IsTracked and k == 1 and  k2 <= 2 and " Drive" or ""
+				local Road = IsTracked and k == 1 and k2 > 2 and " Road" or ""
+				Name = Direction .. Drive .. Steer .. Road .. " Wheel"
+
+				DrawEntLink(v, w, Color(255, 255, 0))
+				DrawEntText(w, Name, Color(255, 0, 0))
+			end
+		end
 	end
 
 	function TOOL:DrawToolScreen()
@@ -158,6 +181,9 @@ if CLIENT then
 	function TOOL:RightClick(Trace) return true end
 	function TOOL:Reload(Trace) return true end
 elseif SERVER then -- Serverside-only stuff
+	util.AddNetworkString("ACF_Sus_Tool")
+	util.AddNetworkString("ACF_Sus_Tool_Server")
+
 	local function checkOwner(ply, ent)
 		if CPPI then
 			local owner = ent:CPPIGetOwner() or (ent.GetPlayer and ent:GetPlayer())
@@ -185,69 +211,127 @@ elseif SERVER then -- Serverside-only stuff
 		local Stage = self:GetStage()
 		if IsShift then
 			if Stage == 0 then
-				print("Selected Baseplate")
-				self.Baseplate = Ent
-				self:SelectColor(Ent, Color(255, 255, 255))
+				self.Selections.Plates[1] = Ent
+				self.Selections.PlatesToWheels[Ent] = {}
 				self:SetStage(1)
 			elseif Stage ~= 0 then
-				print("Selected Steer plate")
-				local CurrentPlate = self.SteerPlates[#self.SteerPlates]
-				self.SteerPlateToWheels[CurrentPlate] = self.SteerWheels
-				self.SteerWheels = {}
-				PrintTable(self.SteerPlateToWheels or {})
-				table.insert(self.SteerPlates, Ent)
-				self:SelectColor(Ent)
-				self:SetStage(6)
+				table.insert(self.Selections.Plates, Ent)
+				self.Selections.PlatesToWheels[Ent] = {}
 			end
 		else
-			if Stage == 1 then
-				print("Selected Left Drive Wheel")
-				self.DriveWheels[1] = Ent
-				self:SelectColor(Ent, Color(255, 0, 0))
-				self:SetStage(2)
-			elseif Stage == 2 then
-				print("Selected Right Drive Wheel")
-				self.DriveWheels[2] = Ent
-				self:SelectColor(Ent, Color(255, 0, 0))
-				self:SetStage(3)
-			elseif Stage == 3 then
-				print("Selected Left Idler Wheel")
-				self.IdlerWheels[1] = Ent
-				self:SelectColor(Ent, Color(255, 0, 0))
-				self:SetStage(4)
-			elseif Stage == 4 then
-				print("Selected Right Idler Wheel")
-				self.IdlerWheels[2] = Ent
-				self:SelectColor(Ent, Color(255, 0, 0))
-				self:SetStage(5)
-			elseif Stage == 5 then
-				print("Selected Road Wheel")
-				table.insert(self.RoadWheels, Ent)
-				self:SelectColor(Ent, Color(0, 255, 0))
-			elseif Stage == 6 then
-				print("Selected Steer Wheel")
-				table.insert(self.SteerWheels, Ent)
-				
-				self:SelectColor(Ent, Color(0, 0, 255))
+			if Stage ~= 0 then
+				table.insert(self.Selections.Wheels, Ent)
+				local CurrentPlate = self.Selections.Plates[#self.Selections.Plates]
+				table.insert(self.Selections.PlatesToWheels[CurrentPlate], Ent)
 			end
 		end
+
+		-- Broadcast the selections to the client
+		net.Start("ACF_Sus_Tool")
+		net.WriteTable(self.Selections)
+		net.Send(Player)
 		return true
 	end
 
 	function TOOL:Reload(Trace)
+		local Player = self:GetOwner()
 		print("Reset stage")
 		self:SetStage(0)
 
-		for Ent in pairs(self.ColMatMap) do
-			if IsValid(Ent) then self:UnselectColor(Ent) end
-		end
-		self.Baseplate = nil
-		self.DriveWheels = {}
-		self.IdlerWheels = {}
-		self.RoadWheels = {}
-		self.SteerPlates = {}
-		self.SteerWheels = {}
-		self.SteerPlateToWheels = {}
+		self.Selections = {}
+		self.Selections.Plates = {}
+		self.Selections.Wheels = {}
+		self.Selections.PlatesToWheels = {}
+
+		-- Broadcast the selections to the client
+		net.Start("ACF_Sus_Tool")
+		net.WriteTable(self.Selections)
+		net.Send(Player)
 		return true
+	end
+
+	net.Receive("ACF_Sus_Tool_Server", function()
+		local Action = net.ReadString()
+		local Player = net.ReadEntity()
+		local Tool = Player:GetTool("acfsuspension")
+
+		if Action == "Create" then Tool:CreateSuspension()
+		elseif Action == "Clear" then Tool:ClearSuspension()
+		end
+	end)
+
+	-- Tolerances for adv ballsocket constraints
+	local MinTol = -0.001
+	local MaxTol = 0.001
+
+	--- Creates an axis center constraint between the wheel and the palte
+	--- This forces the wheel to rotate in place, in the forward/backward direction
+	local function Axis(Wheel, Plate)
+		return constraint.Axis(Wheel, Plate, 0, 0, Vector(0, 0, 0), Vector(0, 0, 0), 0, 0, 0, 0, Vector(0, 1, 0), false)
+	end
+
+	--- Creates a adv ballsocket constraint between the wheel and the plate
+	--- This forces the wheel to rotate freely, in the forward/backward direction
+	local function HullSocket(Wheel, Plate)
+		return constraint.AdvBallsocket(Wheel, Plate, 0, 0, Vector(0, 0, 0), Vector(0, 0, 0), 0, 0, -180, 180, MinTol, MaxTol, MaxTol, MaxTol, 0, 0, 0, 1, 0)
+	end
+
+	--- Creates a adv ballsocket constraint between the wheel and its drive wheel
+	--- This forces the wheel to rotate freely, with the drive wheel
+	local function SlaveSocket(Wheel, DriveWheel)
+		return constraint.AdvBallsocket( Wheel, DriveWheel, 0, 0, Vector(0, 0, 0), Vector(0, 0, 0), 0, 0, MinTol, MinTol, MinTol, MaxTol, MaxTol, MaxTol, 0, 0, 0, 1, 0 )
+	end
+
+	--- Creates a simple ballsocket constraint between the wheel and the plate
+	--- This forces the wheel to rotate in place, in any direction
+	local function RotationSocket(Wheel, Plate)
+		return constraint.Ballsocket(Wheel, Plate, 0, 0, Vector(0, 0, 0), 0, 0, 0)
+	end
+
+	function TOOL:CreateSuspension()
+		print("Create Suspension")
+		local Player = self:GetOwner()
+		local Selections = self.Selections
+
+		local IsTracked = GetConVar("ACF_Sus_Tool_IsTracked"):GetInt()
+
+		local Baseplate = Selections.Plates[1]
+		local LeftDriveWheel = Selections.Wheels[1]
+		local RightDriveWheel = Selections.Wheels[2]
+		if IsTracked then -- Tracked
+			for Index, Wheel in ipairs(Selections.PlatesToWheels[Baseplate] or {}) do
+				if not IsValid(Wheel) then continue end
+				if Index > 2 then SlaveSocket(Wheel, Index % 2 == 1 and LeftDriveWheel or RightDriveWheel) end
+				Axis(Wheel, Baseplate)
+			end
+		else -- Wheeled
+			for Index, Plate in ipairs(Selections.Plates) do
+				if not IsValid(Plate) then continue end
+				for _, Wheel in ipairs(Selections.PlatesToWheels[Plate] or {}) do
+					if not IsValid(Wheel) then continue end
+					if Index == 1 then
+						Axis(Wheel, Plate)
+					else
+						HullSocket(Wheel, Plate)
+						RotationSocket(Wheel, Plate)
+					end
+				end
+			end
+		end
+
+		ACF.SendNotify(Player, true, "Created " .. (IsTracked and "tracked" or "wheeled") .. " drivetrain")
+	end
+
+	function TOOL:ClearSuspension()
+		print("Clear Suspension")
+		local Player = self:GetOwner()
+		local Selections = self.Selections
+		for _, v in pairs(Selections.Plates) do
+			if IsValid(v) then constraint.RemoveAll(v) end
+		end
+		for _, v in pairs(Selections.Wheels) do
+			if IsValid(v) then constraint.RemoveAll(v) end
+		end
+		ACF.SendNotify(Player, true, "Cleared all constraints in drivetrain")
 	end
 end
