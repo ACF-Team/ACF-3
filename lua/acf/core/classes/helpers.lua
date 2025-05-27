891b7a5c507fd072511d9a4d38a9b817f5d16ad2
march <106459595+marchc1@users.noreply.github.com>
2025-05-26 18:33:46 -0700

    Object-oriented API for LimitConVar
    We need RemoveCount for missiles, which doesnt exist in base game
    Albeit maybe this solves a problem that doesnt exist. Not sure

--- lua/acf/core/classes/helpers.lua
+++ lua/acf/core/classes/helpers.lua
@@ -4,10 +4,34 @@ local Classes = ACF.Classes
 --- @param Data {Name:string, Amount:number, Text:string}
 function Classes.AddSboxLimit(Data)
 	if CLIENT then return end
-	if ConVarExists("sbox_max" .. Data.Name) then return end
 
-	CreateConVar("sbox_max" .. Data.Name,
+	local ConVarName = "sbox_max" .. Data.Name
+
+	if ConVarExists(ConVarName) then return end
+
+	CreateConVar(ConVarName,
 				Data.Amount,
 				FCVAR_ARCHIVE + FCVAR_NOTIFY,
 				Data.Text or "")
+
+	function Data:AddCount(Ply, Ent)
+		return Ply:AddCount(self.Name, Ent)
+	end
+
+	function Data:RemoveCount(Ply, Ent)
+		local SBoxObjects = _G.g_SBoxObjects
+		local Key = Ply:UniqueID()
+		local Str = self.Name
+
+		SBoxObjects[Key] = SBoxObjects[Key] or {}
+		SBoxObjects[Key][Str] = SBoxObjects[Key][Str] or {}
+
+		-- Why does gmod store it like this :(
+		table.RemoveByValue(SBoxObjects[Key][Str], Ent)
+		Ply:GetCount(Str)
+	end
+
+	function Data:CheckLimit(Ply)
+		return Ply:CheckLimit(self.Name)
+	end
 end
