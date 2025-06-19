442cc284ecddc343c218d08c8c8df3158fc287bb
march <106459595+marchc1@users.noreply.github.com>
2025-06-18 20:45:11 -0700

    Add player check, error if not player, give details

--- lua/acf/core/utilities/util_sh.lua
+++ lua/acf/core/utilities/util_sh.lua
@@ -1119,6 +1119,9 @@ do -- Reload related
 	--- @param Model any The model of the seat
 	--- @return unknown Pod The generated seat
 	function ACF.GenerateLuaSeat(Entity, Player, Pos, Angle, Model)
+		if not IsValid(Player) or not Player:IsPlayer() then
+			error("Player was not valid or was a different type (got " .. (Player and type(Player) or "<nil>"))
+		end
 		if not Player:CheckLimit("vehicles") then return end
 
 		-- print("GenerateLuaSeat", Entity, Player, Pos, Angle, Model)
