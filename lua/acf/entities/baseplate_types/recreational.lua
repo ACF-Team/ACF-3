1107854352b6e889b8871ab00a8da82725d6d820
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2025-07-22 21:37:17 -0400

    Finish up recreational baseplates

--- lua/acf/entities/baseplate_types/recreational.lua
+++ lua/acf/entities/baseplate_types/recreational.lua
@@ -5,32 +5,45 @@ local EmptyTable = {}
 function Baseplate:OnLoaded()
 	self.Name		 = "Recreational"
 	self.Icon        = "icon16/car_add.png"
-	self.Description = "A baseplate designed for non combat use (e.g. cars).\nWeapons and ammo will be disabled. Fuel consumption rate set to 1."
+	self.Description = "A baseplate designed for non combat use (e.g. cars).\nWeapons and ammo will be removed. Fuel consumption rate set to 1."
 	self.EnforceLooped = function(Baseplate)
 		local Contraption = Baseplate:GetContraption()
 		if not Contraption then return end
 
-		-- Disable ammo
+		local Owner = Baseplate:CPPIGetOwner()
+
+		-- Kill ammo
 		for v, _ in pairs(Contraption.Ammo or EmptyTable) do
-			if not v.Disabled then ACF.DisableEntity(v, "Recreational Used For Combat", "Recreational Baseplate Used For Combat", 100) end
+			if IsValid(Owner) then ACF.SendNotify(Owner, false, "Recreational Baseplate Used For Combat") end
+			if IsValid(v) then
+				Contraption.Ammo[v] = nil
+				v:Remove()
+			end
 		end
 
-		-- Recreational baseplates should not refuel others
-		for v, _ in pairs(Contraption.Fuels or EmptyTable) do
-			v.SupplyFuel = false
-		end
-
-		-- Disable weapons
+		-- Kill weapons
 		for v, _ in pairs(Contraption.Guns or EmptyTable) do
-			if not v.Disabled then ACF.DisableEntity(v, "Recreational Used For Combat", "Recreational Baseplate Used For Combat", 100) end
+			if IsValid(Owner) then ACF.SendNotify(Owner, false, "Recreational Baseplate Used For Combat") end
+			if IsValid(v) then
+				Contraption.Guns[v] = nil
+				v:Remove()
+			end
 		end
 
 		for v, _ in pairs(Contraption.Piledrivers or EmptyTable) do
-			if not v.Disabled then ACF.DisableEntity(v, "Recreational Used For Combat", "Recreational Baseplate Used For Combat", 100) end
+			if IsValid(Owner) then ACF.SendNotify(Owner, false, "Recreational Baseplate Used For Combat") end
+			if IsValid(v) then
+				Contraption.Piledrivers[v] = nil
+				v:Remove()
+			end
 		end
 
 		for v, _ in pairs(Contraption.Racks or EmptyTable) do
-			if not v.Disabled then ACF.DisableEntity(v, "Recreational Used For Combat", "Recreational Baseplate Used For Combat", 100) end
+			if IsValid(Owner) then ACF.SendNotify(Owner, false, "Recreational Baseplate Used For Combat") end
+			if IsValid(v) then
+				Contraption.Racks[v] = nil
+				v:Remove()
+			end
 		end
 	end
 end
