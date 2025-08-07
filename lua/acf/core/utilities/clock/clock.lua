06729f8769c490e38188092d6922c1ca844df547
march <106459595+marchc1@users.noreply.github.com>
2025-08-07 00:48:38 -0700

    Curtime experiment; double precision
    We'll see if this works... we just need this in Lua-land for delta comparisons and a few other things.
    
    I'm curious if it will fix other problems as well.
    It might also just be completely redundant. We'll see

--- lua/acf/core/utilities/clock/clock.lua
+++ lua/acf/core/utilities/clock/clock.lua
@@ -1,10 +1,16 @@
 local Clock = ACF.Utilities.Clock
 
+-- This is weird, but in theory, we can get a pseudo-double-curtime with this, suitable for clock operations.
+-- I mean in theory, gpGlobals curtime is calculated this way... right?
+local function DoubleCurtime()
+	return engine.TickCount() * engine.TickInterval()
+end
+
 Clock.DeltaTime = engine.TickInterval()
-Clock.CurTime   = CurTime()
+Clock.CurTime   = DoubleCurtime()
 
 hook.Add("Think", "ACF Clock Update", function()
-	local Now   = CurTime()
+	local Now   = DoubleCurtime()
 	local Delta = Now - Clock.CurTime
 
 	Clock.DeltaTime = Delta
