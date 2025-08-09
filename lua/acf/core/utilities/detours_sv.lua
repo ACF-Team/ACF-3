3c5b42cf1d001c91f9b582da95e0e1673899a062
march <106459595+marchc1@users.noreply.github.com>
2025-08-09 14:56:04 -0700

    Lets try detours again

--- lua/acf/core/utilities/detours_sv.lua
+++ lua/acf/core/utilities/detours_sv.lua
@@ -1,17 +1,17 @@
--- -- Registers a bunch of legality detours.
+-- Registers a bunch of legality detours.
 
--- local Detours = ACF.Detours
+local Detours = ACF.Detours
 
--- timer.Simple(Detours.Loaded and 0 or 5, function()
---     Detours.Loaded = true
+timer.Simple(Detours.Loaded and 0 or 5, function()
+    Detours.Loaded = true
 
---     local E2_SetPos E2_SetPos = Detours.Expression2("e:setPos(v)", function(Scope, Args, ...)
---         return E2_SetPos(Scope, Args, ...)
---     end)
+    local E2_SetPos E2_SetPos = Detours.Expression2("e:setPos(v)", function(Scope, Args, ...)
+        return E2_SetPos(Scope, Args, ...)
+    end)
 
---     local SF_SetPos SF_SetPos = Detours.Starfall("instance.Types.Entity.Methods.setPos", function(_, Entity, Pos, ...)
---         return SF_SetPos(Entity, Pos, ...)
---     end)
+    local SF_SetPos SF_SetPos = Detours.Starfall("instance.Types.Entity.Methods.setPos", function(Instance, ...)
+        return SF_SetPos(Instance, ...)
+    end)
 
---     print("ACF detours loaded...") -- I forgot the print function :) note to self to find it
--- end)
+    print("ACF detours loaded...") -- I forgot the print function :) note to self to find it
+end)
