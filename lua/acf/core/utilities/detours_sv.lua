1bd3c3bfde6e15e88bcfb538333c897704ec6172
march <106459595+marchc1@users.noreply.github.com>
2025-08-09 15:51:38 -0700

    E2 detour this too

--- lua/acf/core/utilities/detours_sv.lua
+++ lua/acf/core/utilities/detours_sv.lua
@@ -36,6 +36,8 @@ timer.Simple(Detours.Loaded and 0 or 5, function()
     Detours.Loaded = true
 
     local E2_SetAng E2_SetAng = Detours.Expression2("e:setAng(a)", function(Scope, Args, ...)
+        if not CanSetAng(Args[1]) then return end
+        print("Can!")
         return E2_SetAng(Scope, Args, ...)
     end)
     local SF_SetAng SF_SetAng = Detours.Starfall("instance.Types.Entity.Methods.setAngles", function(Instance, Ent, ...)
