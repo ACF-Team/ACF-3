9b6e3c6cd501d76937a0e2f0315b11394dcf90ba
march <106459595+marchc1@users.noreply.github.com>
2025-08-14 14:55:27 -0700

    Don't block setAng, but still disable the family
    This should help with user-tools. It will still be a little annoying, but they will work.

--- lua/acf/core/utilities/detours_sv.lua
+++ lua/acf/core/utilities/detours_sv.lua
@@ -35,11 +35,11 @@ timer.Simple(Detours.Loaded and 0 or 5, function()
     Detours.Loaded = true
 
     local E2_SetAng E2_SetAng = Detours.Expression2("e:setAng(a)", function(Scope, Args, ...)
-        if not CanSetAng(Args[1]) then return end
+        CanSetAng(Args[1])
         return E2_SetAng(Scope, Args, ...)
     end)
     local SF_SetAng SF_SetAng = Detours.Starfall("instance.Types.Entity.Methods.setAngles", function(Instance, Ent, ...)
-        if not CanSetAng(Instance.Types.Entity.Unwrap(Ent)) then return end
+        CanSetAng(Instance.Types.Entity.Unwrap(Ent))
         return SF_SetAng(Instance, Ent, ...)
     end)
 
