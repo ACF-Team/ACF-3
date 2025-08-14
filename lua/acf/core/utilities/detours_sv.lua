db645a93c94a45fbf36b04ec83e94082029d8f79
march <106459595+marchc1@users.noreply.github.com>
2025-08-14 15:51:35 -0700

    Add new detours for setpos, physobj methods

--- lua/acf/core/utilities/detours_sv.lua
+++ lua/acf/core/utilities/detours_sv.lua
@@ -23,25 +23,65 @@ local function PreCheck()
     if not ACF.LegalChecks then return true end
 end
 
-local function CanSetAng(Ent)
+local function BlockEntManipulationIfApplicable(Ent, Type)
     if PreCheck() then return true end
     if Ent.IsACFEntity then
-        return DisableFamily(Ent, "Attempted to call SetAngles via a blocked usercall.")
+        return DisableFamily(Ent, ("Attempted to call %s (a blocked usercall)."):format(Type or "UNKNOWN"))
     end
+    return true
+end
+
+local function BlockPhysObjManipulationIfApplicable(PhysObj, Type)
+    if PreCheck() then return true end
+    if not IsValid(PhysObj) then return false end
+    local Ent = PhysObj:GetEntity()
+    if not IsValid(Ent) then return false end
+
+    if Ent.IsACFEntity then
+        return DisableFamily(Ent, ("Attempted to call %s (a blocked usercall)."):format(Type or "UNKNOWN"))
+    end
+
     return true
 end
 
 timer.Simple(Detours.Loaded and 0 or 5, function()
     Detours.Loaded = true
 
-    local E2_SetAng E2_SetAng = Detours.Expression2("e:setAng(a)", function(Scope, Args, ...)
-        CanSetAng(Args[1])
-        return E2_SetAng(Scope, Args, ...)
-    end)
-    local SF_SetAng SF_SetAng = Detours.Starfall("instance.Types.Entity.Methods.setAngles", function(Instance, Ent, ...)
-        CanSetAng(Instance.Types.Entity.Unwrap(Ent))
-        return SF_SetAng(Instance, Ent, ...)
-    end)
+    -- DETOURS: SetPos
+    do
+        local E2_Ent_SetPos E2_Ent_SetPos = Detours.Expression2("e:setPos(v)", function(Scope, Args, ...)
+            BlockEntManipulationIfApplicable(Args[1], "e:setPos(v)")
+            return E2_Ent_SetPos(Scope, Args, ...)
+        end)
+
+        local SF_Ent_SetPos SF_Ent_SetPos = Detours.Starfall("instance.Types.Entity.Methods.setPos", function(Instance, Ent, ...)
+            BlockEntManipulationIfApplicable(Instance.Types.Entity.Unwrap(Ent), "e:setPos(v)")
+            return SF_Ent_SetPos(Instance, Ent, ...)
+        end)
+
+        local SF_PhysObj_SetPos SF_PhysObj_SetPos = Detours.Starfall("instance.Types.PhysObj.Methods.setPos", function(Instance, PhysObj, ...)
+            BlockPhysObjManipulationIfApplicable(Instance.Types.PhysObj.Unwrap(PhysObj), "physobj:setPos(v)")
+            return SF_PhysObj_SetPos(Instance, PhysObj, ...)
+        end)
+    end
+
+    -- DETOURS: SetAng
+    do
+        local E2_Ent_SetAng E2_Ent_SetAng = Detours.Expression2("e:setAng(a)", function(Scope, Args, ...)
+            BlockEntManipulationIfApplicable(Args[1], "e:setAng(a)")
+            return E2_Ent_SetAng(Scope, Args, ...)
+        end)
+
+        local SF_Ent_SetAng SF_Ent_SetAng = Detours.Starfall("instance.Types.Entity.Methods.setAngles", function(Instance, Ent, ...)
+            BlockEntManipulationIfApplicable(Instance.Types.Entity.Unwrap(Ent), "e:setAng(a)")
+            return SF_Ent_SetAng(Instance, Ent, ...)
+        end)
+
+        local SF_PhysObj_SetAng SF_PhysObj_SetAng = Detours.Starfall("instance.Types.PhysObj.Methods.setAng", function(Instance, PhysObj, ...)
+            BlockPhysObjManipulationIfApplicable(Instance.Types.PhysObj.Unwrap(PhysObj), "physobj:setAng(a)")
+            return SF_PhysObj_SetAng(Instance, PhysObj, ...)
+        end)
+    end
 
     print("ACF detours loaded...") -- I forgot the print function :) note to self to find it
 end)
