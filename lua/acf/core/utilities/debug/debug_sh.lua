234cf41d4b1c648aad21742d7576ef37c2924c1f
march <106459595+marchc1@users.noreply.github.com>
2025-08-15 18:58:57 -0700

    Add acf_physicsvistest

--- lua/acf/core/utilities/debug/debug_sh.lua
+++ lua/acf/core/utilities/debug/debug_sh.lua
@@ -262,3 +262,143 @@ local function t(test)
 	ACF.DumpStack()
 end
 t()]]
+
+if SERVER then ACF_PHYSICSVISTEST_LASTREQUESTEDENTS = ACF_PHYSICSVISTEST_LASTREQUESTEDENTS or {} end
+local RequestedEnts = SERVER and ACF_PHYSICSVISTEST_LASTREQUESTEDENTS or nil
+
+local function ProcessLastTyped(Player, Request)
+	local LUT = {}
+	local Pieces = string.Split(Request, " ")
+	for _, Piece in ipairs(Pieces) do
+		local Idx = tonumber(Piece)
+		if Idx then LUT[Idx] = true end
+	end
+
+	if SERVER then
+		if #Pieces == 0 then
+			RequestedEnts[Player] = nil
+		else
+			RequestedEnts[Player] = LUT
+		end
+		Player:ChatPrint("Now tracking: " .. table.concat(table.GetKeys(LUT), ", "))
+	else
+		if #Pieces == 0 then
+			RequestedEnts = nil
+		else
+			RequestedEnts = LUT
+		end
+	end
+end
+
+if SERVER then
+	concommand.Add("acf_physicsvistest", function(Player, _, _, Request)
+		if Player == NULL then print("Cannot use this command from a dedicated server!") return end
+		if not FunDebuggingFunctions:GetBool() then Player:ChatPrint("Fun debugging functions aren't enabled...") return end
+		ProcessLastTyped(Player, Request)
+	end, nil, "ACF physics visualizer for testing", FCVAR_USERINFO)
+end
+
+if SERVER then
+	util.AddNetworkString("ACF_PhysVisData")
+	local LastUpdateTime = 0
+	local function DoPhysVis()
+		local Now = CurTime()
+		local Delta = Now - LastUpdateTime
+		if Delta > 0.05 then
+			for Player, LUT in pairs(RequestedEnts) do
+				if IsValid(Player) then
+					net.Start("ACF_PhysVisData")
+					net.WriteUInt(table.Count(LUT), 6)
+					for EntIdx in pairs(LUT) do
+						local Ent = Entity(EntIdx)
+						if not IsValid(Ent) then net.WriteBool(false) continue end
+
+						net.WriteBool(true)
+						net.WriteUInt(EntIdx, MAX_EDICT_BITS)
+						local PhysicsObjects = math.min(Ent:GetPhysicsObjectCount(), 63)
+						net.WriteUInt(PhysicsObjects, 6)
+						for i = 1, PhysicsObjects do
+							local PhysObj = Ent:GetPhysicsObjectNum(i - 1)
+							if not IsValid(PhysObj) then net.WriteBool(false) continue end
+							net.WriteBool(true)
+							net.WriteUInt(i, 6)
+							net.WriteVector(Ent:WorldToLocal(PhysObj:GetPos()))
+							net.WriteAngle(Ent:WorldToLocalAngles(PhysObj:GetAngles()))
+							net.WriteFloat(PhysObj:GetMass())
+							local IS, ES = PhysObj:GetStress()
+							net.WriteFloat(IS)
+							net.WriteFloat(ES)
+						end
+					end
+					net.Send(Player)
+				end
+			end
+			LastUpdateTime = Now
+		end
+	end
+
+	local function EvaluateCurrentDebuggingState(Value)
+		if (tonumber(Value) or 0) >= 1 then
+			hook.Add("Think", "ACF_FunDebuggingFuncs_PhysVis", DoPhysVis)
+		else
+			hook.Remove("Think", "ACF_FunDebuggingFuncs_PhysVis")
+		end
+	end
+
+	cvars.AddChangeCallback("acf_fundebuggingfuncs", function(_, _, Value)
+		EvaluateCurrentDebuggingState(Value)
+	end)
+
+	EvaluateCurrentDebuggingState(FunDebuggingFunctions:GetString())
+else
+	local PhysData = {}
+	net.Receive("ACF_PhysVisData", function()
+		table.Empty(PhysData)
+		local Ents = net.ReadUInt(6)
+		for _ = 1, Ents do
+			local Valid = net.ReadBool()
+			if not Valid then continue end
+
+			local LUT = {}
+			PhysData[net.ReadUInt(MAX_EDICT_BITS)] = LUT
+			local Objects = net.ReadUInt(6)
+			for PhysIdx = 1, Objects do
+				local ValidPhys = net.ReadBool()
+				LUT[PhysIdx] = {
+					ValidPhys = ValidPhys,
+					Index = ValidPhys and net.ReadUInt(6),
+					Position = ValidPhys and net.ReadVector(),
+					Angles = ValidPhys and net.ReadAngle(),
+					Mass = ValidPhys and net.ReadFloat(),
+					InternalStress = ValidPhys and net.ReadFloat(),
+					ExternalStress = ValidPhys and net.ReadFloat(),
+				}
+			end
+		end
+	end)
+
+	surface.CreateFont("ACF_DebugFixed", {
+		font = "Consolas",
+		size = 14,
+		weight = 900
+	})
+	hook.Add("HUDPaint", "ACF_FunDebuggingFuncs_PhysVis", function()
+		if not FunDebuggingFunctions:GetBool() then return end
+
+		for EntIdx, EntPhys in pairs(PhysData) do
+			local Ent = Entity(EntIdx)
+			if not IsValid(Ent) then continue end
+
+			for _, PhysObj in ipairs(EntPhys) do
+				local Pos = Ent:LocalToWorld(PhysObj.Position)
+				local Ang = Ent:LocalToWorldAngles(PhysObj.Angles)
+
+				local ScreenPos = Pos:ToScreen()
+				draw.SimpleTextOutlined("Idx             : " .. PhysObj.Index, "ACF_DebugFixed", ScreenPos.x, ScreenPos.y, color_White, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, 1, color_black)
+				draw.SimpleTextOutlined("Mass            : "   .. PhysObj.Mass, "ACF_DebugFixed", ScreenPos.x, ScreenPos.y + 16, color_White, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, 1, color_black)
+				draw.SimpleTextOutlined("Internal Stress : "   .. PhysObj.InternalStress, "ACF_DebugFixed", ScreenPos.x, ScreenPos.y + 32, color_White, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, 1, color_black)
+				draw.SimpleTextOutlined("External Stress : "   .. PhysObj.ExternalStress, "ACF_DebugFixed", ScreenPos.x, ScreenPos.y + 48, color_White, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, 1, color_black)
+			end
+		end
+	end)
+end
