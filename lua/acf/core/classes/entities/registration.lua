18d0a7c76c6280e644910b55cfa9ef7c5e4ef22f
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2025-08-29 18:52:14 -0400

    Fix weird baseplate seat bug in autoreg
    I guess we really do need First

--- lua/acf/core/classes/entities/registration.lua
+++ lua/acf/core/classes/entities/registration.lua
@@ -372,7 +372,7 @@ function Entities.AutoRegister(ENT)
 	--- Updates the entity's user vars with ClientData
 	--- @param self table The entity to update
 	--- @param ClientData table The client data to use for the update
-	local function UpdateEntityData(self, ClientData)
+	local function UpdateEntityData(self, ClientData, First)
 		local Entity = GetEntityTable(Class) -- THE ENTITY TABLE, NOT THE ENTITY ITSELF
 		local List   = Entity.List
 
@@ -382,7 +382,7 @@ function Entities.AutoRegister(ENT)
 
 		-- For entity arguments that are marked as client data, set them on the entity from ClientData
 		for _, v in ipairs(List) do
-			if UserVars[v].ClientData then
+			if UserVars[v].ClientData or First then
 				self.ACF_UserData[v] = ClientData[v]
 			end
 		end
@@ -473,7 +473,7 @@ function Entities.AutoRegister(ENT)
 
 		hook.Run("ACF_OnSpawnEntity", Class, New, ClientData)
 
-		New:ACF_UpdateEntityData(ClientData)
+		New:ACF_UpdateEntityData(ClientData, true)
 		if New.ACF_PostSpawn then
 			New:ACF_PostSpawn(Player, Pos, Angle, ClientData)
 		end
