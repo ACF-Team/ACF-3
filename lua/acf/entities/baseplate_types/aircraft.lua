a5216dce3a2ea19c9763024a2f90c732c6b52d85
march <106459595+marchc1@users.noreply.github.com>
2025-11-02 22:24:13 -0800

    Null check just in case

--- lua/acf/entities/baseplate_types/aircraft.lua
+++ lua/acf/entities/baseplate_types/aircraft.lua
@@ -19,14 +19,16 @@ function Baseplate:PhysicsCollide(Data)
     if Data.Speed > 1000 then
         -- Timer simple to avoid "Changing collision rules within a callback is likely to cause crashes!"
         timer.Simple(0, function()
-            local Position = self:GetPos()
+            local Position = IsValid(self) and self:GetPos() or nil
             for Player in ACF.PlayersInContraptionIterator(Contraption) do
                 Player:Kill()
             end
             for Entity in pairs(Contraption.ents) do
                 ACF.HEKill(Entity, Data.HitNormal, Data.Speed * 100, Data.HitPos, nil, true)
             end
-            ACF.Damage.explosionEffect(Position, Data.HitNormal, 120)
+            if Position then
+                ACF.Damage.explosionEffect(Position, Data.HitNormal, 120)
+            end
         end)
     end
 end
