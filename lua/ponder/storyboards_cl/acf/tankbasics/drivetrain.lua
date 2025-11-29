7fd4b27125e437bc6e2e5f346aa2522e3c96b0f0
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2025-11-28 20:31:18 -0500

    Remove placeholder ponders

--- lua/ponder/storyboards_cl/acf/tankbasics/drivetrain.lua
+++
@@ -1,8 +0,0 @@
--- local Storyboard = Ponder.API.NewStoryboard("acf", "tankbasics", "drivetrain")
--- Storyboard:WithName("Drivetrains")
--- Storyboard:WithBaseEntity(nil)
--- Storyboard:WithModelIcon("models/engines/transaxial_s.mdl")
--- Storyboard:WithDescription("Learn how to make your tank move")
--- Storyboard:WithIndexOrder(5)
-
--------------------------------------------------------------------------------------------------
