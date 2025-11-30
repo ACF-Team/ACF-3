048521b0106e769ef919e9c79d14b19a0b0c170c
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2025-11-30 06:20:13 -0500

    WIP

--- lua/acf/contraption/track_cost.lua
+++ lua/acf/contraption/track_cost.lua
@@ -1,23 +1,65 @@
-local EntityCost = 0.1 -- Cost per entity in contraption
 
 hook.Add("cfw.contraption.created", "ACF_CFW_CostTrack", function(Contraption)
-    print("cfw.contraption.created", Contraption)
-    Contraption.Cost = 0
+	print("cfw.contraption.created", Contraption)
+	Contraption.Cost = 0
+	Contraption.AmmoTypes = {}
+	Contraption.MaxPenRound = 0
+	Contraption.MaxNominalArmor = 0
 end)
 
 hook.Add("cfw.contraption.entityAdded", "ACF_CFW_CostTrack", function(Contraption, Entity)
-    print("cfw.contraption.entityAdded", Contraption, Entity)
-    Contraption.Cost = Contraption.Cost + EntityCost
+	-- print("cfw.contraption.entityAdded", Contraption, Entity)
+	local PhysObj = Entity:GetPhysicsObject()
+	Contraption.Cost = Contraption.Cost + 0.1 + (IsValid(PhysObj) and math.max(0.01, PhysObj:GetMass() / 500) or 0)
+
+	if Entity.IsACFEntity and Entity.IsACFAmmoCrate then
+		Contraption.AmmoTypes[Entity.AmmoType] = true
+		-- Contraption.MaxPenRound = math.max(Contraption.MaxPenRound, Entity.AmmoData and Entity.AmmoData.MinPen or 0)
+	end
 end)
 
 hook.Add("cfw.contraption.entityRemoved", "ACF_CFW_CostTrack", function(Contraption, Entity)
-    print("cfw.contraption.entityRemoved", Contraption, Entity)
+	print("cfw.contraption.entityRemoved", Contraption, Entity)
 end)
 
 hook.Add("cfw.contraption.merged", "ACF_CFW_CostTrack", function(Contraption, MergedInto)
-    print("cfw.contraption.merged", Contraption, MergedInto)
+	print("cfw.contraption.merged", Contraption, MergedInto)
 end)
 
 hook.Add("cfw.contraption.removed", "ACF_CFW_CostTrack", function(Contraption)
-    print("cfw.contraption.removed", Contraption)
+	print("cfw.contraption.removed", Contraption)
 end)
+
+if CLIENT then
+	net.Receive("ReqContraption", function()
+		local Entity = net.ReadEntity()
+		Entity.Name = net.ReadString()
+		Entity.BaseplateType = net.ReadString()
+		Entity.Cost = math.Round(net.ReadFloat(), 2)
+		Entity.Count = net.ReadUInt(9)
+		Entity.TotalMass = net.ReadUInt(24)
+		-- print("Received Data", Entity)
+	end)
+elseif SERVER then
+	util.AddNetworkString( "ReqContraption" )
+
+	net.Receive("ReqContraption", function(Len, Player)
+		local Entity = net.ReadEntity()
+
+		if not IsValid(Entity) then return end
+		if not Entity.GetContraption then return end
+
+		local Contraption = Entity:GetContraption()
+
+		if not Contraption then return end
+
+		net.Start("ReqContraption")
+		net.WriteEntity(Entity)
+		net.WriteString(Contraption.ACF_Baseplate and Contraption.ACF_Baseplate:ACF_GetUserVar("Name") or "Unknown")
+		net.WriteString(Contraption.ACF_Baseplate and Contraption.ACF_Baseplate:ACF_GetUserVar("BaseplateType").ID or "Unknown")
+		net.WriteFloat(Contraption.Cost or 0)
+		net.WriteUInt(Contraption.count or 0, 9)
+		net.WriteUInt(Contraption.totalMass or 0, 24)
+		net.Send(Player)
+	end)
+end
--- lua/weapons/gmod_tool/stools/acf_menu.lua
+++ lua/weapons/gmod_tool/stools/acf_menu.lua
@@ -7,20 +7,23 @@ if CLIENT then
 	local Sensitive      = Color(255, 0, 0, 50)
 	local NotSoSensitive = Color(255, 255, 0, 50)
 
-	net.Receive("ReqContraption", function()
-		local Entity = net.ReadEntity()
-		local Cost = math.Round(net.ReadFloat(8), 2)
-		Entity.Cost = Cost
-		print("Contraption Cost: ", Cost)
-	end)
-
-	function RenderContraption(Entity, Cost)
+	function RenderContraption(Entity)
 		surface.SetDrawColor(0, 0, 0, 200)
-		surface.DrawRect( 100, 100, 400, 400 )
+		surface.DrawRect( 100, 200, 400, 400 )
 
 		surface.SetTextColor(255, 255, 255, 255)
-		surface.SetTextPos( 300, 300 )
-		surface.DrawText( "Contraption Cost: " .. tostring(Cost) )
+		surface.SetFont("DermaLarge")
+		surface.SetTextPos( 110, 210 )
+		surface.DrawText("Baseplate: " .. tostring(Entity.BaseplateType))
+		surface.SetTextPos( 110, 240 )
+		surface.DrawText("Name: " .. tostring(Entity.Name))
+		surface.SetTextPos( 110, 270 )
+		surface.DrawText("Cost: " .. tostring(Entity.Cost))
+		surface.SetTextPos( 110, 300 )
+		surface.DrawText("Entity Count: " .. tostring(Entity.Count))
+		surface.SetTextPos( 110, 330 )
+		surface.DrawText("Total Mass: " .. tostring(Entity.TotalMass) .. " kg")
+
 	end
 
 	function TOOL:DrawHUD()
@@ -37,7 +40,7 @@ if CLIENT then
 			self.LastEntity = Entity
 		end
 
-		if Entity.Cost then RenderContraption(Entity, Entity.Cost) end
+		if Entity.Cost then RenderContraption(Entity) end
 
 		if not Entity.DrawOverlay then return end
 
@@ -65,23 +68,5 @@ if CLIENT then
 
 			render.DrawWireframeBox(Pos, Ang, Tab.Scale * -0.5, Tab.Scale * 0.5, Tab.Sensitive and Sensitive or NotSoSensitive)
 		end
-	end)
-elseif SERVER then
-	util.AddNetworkString( "ReqContraption" )
-
-	net.Receive("ReqContraption", function(Len, Player)
-		local Entity = net.ReadEntity()
-
-		if not IsValid(Entity) then return end
-		if not Entity.GetContraption then return end
-
-		local Contraption = Entity:GetContraption()
-
-		if not Contraption then return end
-
-		net.Start("ReqContraption")
-		net.WriteEntity(Entity)
-		net.WriteFloat(Contraption.Cost or 0, 8)
-		net.Send(Player)
 	end)
 end
