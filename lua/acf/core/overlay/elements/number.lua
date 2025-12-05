9198147867634b285217a75ab201729700bef1de
march <106459595+marchc1@users.noreply.github.com>
2025-12-04 18:03:34 -0800

    Update number.lua

--- lua/acf/core/overlay/elements/number.lua
+++ lua/acf/core/overlay/elements/number.lua
@@ -2,7 +2,25 @@ local Overlay = ACF.Overlay
 local ELEMENT = {}
 
 function ELEMENT.Render(_, Slot)
-    Overlay.SimpleText(Slot.Data[1], Overlay.MAIN_FONT, 0, 0, Overlay.COLOR_TEXT, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
+    -- Our horizontal positions here are dependent on the final size of everything.
+    -- So those will be adjusted in PostRender, and we'll allocate our size here.
+
+    local W1, H1 = Overlay.GetTextSize(Overlay.MAIN_FONT, Slot.Data[1] or "Key")
+    local W2, H2 = Overlay.GetTextSize(Overlay.MAIN_FONT, tostring(Slot.Data[2]))
+
+    local W = math.max(W1, W2) + 8
+    local H = math.max(H1, H2)
+
+    -- Allocate our slots size
+    Overlay.AppendSlotSize(W, H)
+    -- For key-values; push Key's size.
+    Overlay.PushKeyWidth(W1)
+end
+
+function ELEMENT.PostRender(_, Slot)
+    Overlay.SimpleText(Slot.Data[1], Overlay.MAIN_FONT, Overlay.GetKVKeyX(), 0, Overlay.COLOR_TEXT, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+    Overlay.DrawKVDivider()
+    Overlay.SimpleText(tostring(Slot.Data[2]), Overlay.MAIN_FONT, Overlay.GetKVValueX(), 0, Overlay.COLOR_TEXT, TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
 end
 
 Overlay.DefineElementType("Number", ELEMENT)
