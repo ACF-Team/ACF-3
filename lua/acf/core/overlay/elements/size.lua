640454eed2370adf5ea2260e782c8516f775c1f3
march <106459595+marchc1@users.noreply.github.com>
2025-12-04 17:25:33 -0800

    Update size.lua

--- lua/acf/core/overlay/elements/size.lua
+++ lua/acf/core/overlay/elements/size.lua
@@ -1,12 +1,23 @@
 local Overlay = ACF.Overlay
 local ELEMENT = {}
 
+local function GetVectorStringFromSlot(Slot)
+    local X, Y, Z
+    if Slot.NumData == 2 then
+        X, Y, Z = Slot.Data[2]:Unpack()
+    else
+        X, Y, Z = Slot.Data[2], Slot.Data[3], Slot.Data[4]
+    end
+
+    return ("%.1f x %.1f x %.1f"):format(X, Y, Z)
+end
+
 function ELEMENT.Render(_, Slot)
     -- Our horizontal positions here are dependent on the final size of everything.
     -- So those will be adjusted in PostRender, and we'll allocate our size here.
 
     local W1, H1 = Overlay.GetTextSize(Overlay.MAIN_FONT, Slot[1] or "Key")
-    local W2, H2 = Overlay.GetTextSize(Overlay.MAIN_FONT, Slot[2] or "Value")
+    local W2, H2 = Overlay.GetTextSize(Overlay.MAIN_FONT, GetVectorStringFromSlot(Slot))
 
     local W = math.max(W1, W2) + 8
     local H = math.max(H1, H2)
@@ -20,7 +31,7 @@ end
 function ELEMENT.PostRender(_, Slot)
     Overlay.SimpleText(Slot.Data[1], Overlay.MAIN_FONT, Overlay.GetKVKeyX(), 0, Overlay.COLOR_TEXT, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
     Overlay.DrawKVDivider()
-    Overlay.SimpleText(Slot.Data[2], Overlay.MAIN_FONT, Overlay.GetKVValueX(), 0, Overlay.COLOR_TEXT, TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
+    Overlay.SimpleText(GetVectorStringFromSlot(Slot), Overlay.MAIN_FONT, Overlay.GetKVValueX(), 0, Overlay.COLOR_TEXT, TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
 end
 
 Overlay.DefineElementType("Size", ELEMENT)
