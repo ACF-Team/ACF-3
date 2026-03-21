7807142ff9d23cdfb75f6fe4c192c0a791f2e6f5
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2026-03-20 23:18:54 -0400

    Fix previous

--- lua/entities/acf_controller/modules/drivetrain.lua
+++ lua/entities/acf_controller/modules/drivetrain.lua
@@ -180,8 +180,8 @@ do
 
 		-- Set default shift RPMs to one of the engine's powerbands
 		local Engine = next(self.Engines)
-		if self:GetShiftMinRPM() == 0 then self:SetShiftMinRPM(Engine.PeakMinRPM + 100) end
-		if self:GetShiftMaxRPM() == 0 then self:SetShiftMaxRPM(Engine.PeakMaxRPM - 100) end
+		if not MainGearbox.Automatic and self:GetShiftMinRPM() == 0 then self:SetShiftMinRPM(Engine.PeakMinRPM + 100) end
+		if not MainGearbox.Automatic and self:GetShiftMaxRPM() == 0 then self:SetShiftMaxRPM(Engine.PeakMaxRPM - 100) end
 
 		self.LastInputs = {}
 		self.LastGear = 0
