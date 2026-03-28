93cd552a822be9cdc875201fbaf4a8595c39544b
march <106459595+marchc1@users.noreply.github.com>
2026-03-28 00:30:06 -0700

    Replace crew death notify message as a test

--- lua/acf/combat/crew_disable_sv.lua
+++ lua/acf/combat/crew_disable_sv.lua
@@ -1,5 +1,6 @@
 -- Purpose: Block crewed contraptions that have had their crew killed from being able to be entered again.
 local ACF = ACF
+local Notify = ACF.Utilities.Notify
 
 hook.Add("CanPlayerEnterVehicle", "ACF_CanPlayerEnterVehicle_BlockEnterVehicleOnDeadContraption", function(Player, Vehicle)
     if not IsValid(Vehicle) then return end
@@ -11,7 +12,13 @@ hook.Add("CanPlayerEnterVehicle", "ACF_CanPlayerEnterVehicle_BlockEnterVehicleOn
 
     if Contraption.ACF_AllCrewKilled then
         if (Now - (Contraption.ACF_LastNotifyDeathTime or 0)) > 1 then
-            ACF.SendNotify(Player, false, "This contraption is no longer usable.")
+            Notify.Start()
+            Notify.WithTitle("This contraption is no longer usable.")
+            Notify.WithSilkIcon("error")
+            Notify.WithTargetEntity(Contraption.ACF_Baseplate)
+            Notify.WithDescription("All crew members have been killed.")
+            Notify.AddPlayer(Player)
+            Notify.Transmit()
             Contraption.ACF_LastNotifyDeathTime = Now
         end
 
