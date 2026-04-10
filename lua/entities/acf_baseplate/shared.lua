b14338c2fcceb3db7eed9de52d48dd355e419710
march <106459595+marchc1@users.noreply.github.com>
2026-04-09 20:41:46 -0700

    oops

--- lua/entities/acf_baseplate/shared.lua
+++ lua/entities/acf_baseplate/shared.lua
@@ -8,10 +8,10 @@ ENT.ACF_Menu_Model = "models/hunter/blocks/cube075x075x075.mdl"
 ENT.ACF_Menu_Description = "Base of all ACF contraptions. Build your vehicle off of this."
 ENT.ACF_Limit = 2
 
-ACFDefineEntVar("Type",         "EnumeratedString",     "ACF.Baseplates.GroundVehicleBaseplate", {MustBeAssignableTo = "ACF.Baseplates.BaseplateType"})
-ACFDefineEntVar("Size",         "Vector",               Vector(144, 72, 1.5),                    {Min = Vector(36, 36, 0.5), Max = Vector(480, 120, 3)})
-ACFDefineEntVar("DisableAltE",  "Bool",                 false,                                   {})
-ACFDefineEntVar("LuaSeat",      "StoredEntity",         nil,                                     {Hidden = true})
+ACF.DefineEntVar("Type",         "EnumeratedString",     "ACF.Baseplates.GroundVehicleBaseplate", {MustBeAssignableTo = "ACF.Baseplates.BaseplateType"})
+ACF.DefineEntVar("Size",         "Vector",               Vector(144, 72, 1.5),                    {Min = Vector(36, 36, 0.5), Max = Vector(480, 120, 3)})
+ACF.DefineEntVar("DisableAltE",  "Bool",                 false,                                   {})
+ACF.DefineEntVar("LuaSeat",      "StoredEntity",         nil,                                     {Hidden = true})
 
 MsgC(SERVER and Color(255, 0, 255) or Color(0, 255, 255), "[ACF] Loaded baseplate entity: " .. ENT.ACF_Class .. "\n")
 
