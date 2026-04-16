27aa64bb23bcff750e11576ed3344e1cc009baf9
march <106459595+marchc1@users.noreply.github.com>
2026-04-15 19:25:33 -0700

    IsValid helpers for specialized types
    IsValid might be too hot for hot paths due to the __index

---
+++ lua/acf/optimizations/is_valid_helpers.lua
@@ -0,0 +1,10 @@
+-- Some helpers for when you know the data-type of what you're validating
+-- Because otherwise, IsValid does an __index call...
+
+local ENTITY     = FindMetaTable("Entity")
+local PLAYER     = FindMetaTable("Player")
+local PHYSOBJ    = FindMetaTable("PhysObj")
+
+function ACF.Optimizations.IsEntityValid(X)  return X and ENTITY.IsValid(X)  end
+function ACF.Optimizations.IsPlayerValid(X)  return X and PLAYER.IsValid(X)  end
+function ACF.Optimizations.IsPhysObjValid(X) return X and PHYSOBJ.IsValid(X) end
