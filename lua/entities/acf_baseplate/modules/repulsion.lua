6f8e49f8060d3ff7f8686e5121820e20b6ee5363
march <106459595+marchc1@users.noreply.github.com>
2026-04-16 12:32:45 -0700

    Optimize baseplate repulsion
    If we're gonna run in a think hook, all these things need to be as fast as possible and not go through __index's

--- lua/entities/acf_baseplate/modules/repulsion.lua
+++ lua/entities/acf_baseplate/modules/repulsion.lua
@@ -1,27 +1,37 @@
-local ACF      		= ACF
-local Clock         = ACF.Utilities.Clock
+local ACF      	      = ACF
+local Clock           = ACF.Utilities.Clock
+
+local IsEntityValid   = ACF.Optimizations.IsEntityValid
+local IsPhysObjValid  = ACF.Optimizations.IsPhysObjValid
+local ENTITY          = FindMetaTable("Entity")
+local VECTOR          = FindMetaTable("Vector")
+local PHYSOBJ         = FindMetaTable("PhysObj")
 
 local function GetBaseplateProperties(Ent, Self, SelfPos, SelfRadius)
 	if Ent == Self then return false end
 
-	if not IsValid(Ent) then return false end
-	if Ent:GetClass() ~= "acf_baseplate" then return false end
+	if not IsEntityValid(Ent) then return false end
+	if ENTITY.GetClass(Ent) ~= "acf_baseplate" then return false end
 	if not Ent.Size then return false end
-	if Ent:IsPlayerHolding() then return false end
+	if ENTITY.IsPlayerHolding(Ent) then return false end
 
-	local Physics     = Ent:GetPhysicsObject()
-	if not IsValid(Physics) then return false end
+	local Physics     = ENTITY.GetPhysicsObject(Ent)
+	if not IsPhysObjValid(Physics) then return false end
 
-	local Pos         = Physics:GetPos()
-	local Radius      = math.sqrt((Ent.Size[1] / 2) ^ 2 + (Ent.Size[2] / 2) ^ 2)
+	local EntTable  = ENTITY.GetTable(Ent)
+
+	local EntX, EntY = VECTOR.Unpack(EntTable.Size)
+
+	local Pos         = PHYSOBJ.GetPos(Physics)
+	local Radius      = math.sqrt((EntX / 2) ^ 2 + (EntY / 2) ^ 2)
 
 	if Self and not util.IsSphereIntersectingSphere(SelfPos, SelfRadius, Pos, Radius) then
 		return false
 	end
 
-	local Vel         = Physics:GetVelocity()
-	local Contraption = Ent:CFW_GetContraption()
-	local PhysMass    = Physics:GetMass()
+	local Vel         = PHYSOBJ.GetVelocity(Physics)
+	local Contraption = ENTITY.CFW_GetContraption(Ent)
+	local PhysMass    = PHYSOBJ.GetMass(Physics)
 	local TotalMass	  = Contraption and Contraption.totalMass or PhysMass
 
 	return true, Physics, Pos, Vel, Contraption, PhysMass, TotalMass, Radius
@@ -29,8 +39,8 @@ end
 
 local function CalculateSphereIntersection(Pos1, Radius1, Pos2, Radius2)
 	local Dir = Pos2 - Pos1
-	local Dist = Dir:Length()
-	Dir:Normalize()
+	local Dist = VECTOR.Length(Dir)
+	VECTOR.Normalize(Dir)
 
 	local Intersection = Dist - Radius1 - Radius2
 	return Intersection, Dir, (Pos1 * Radius1 + Pos2 * Radius2) / (Radius1 + Radius2)
@@ -44,9 +54,10 @@ hook.Add("Think", "ACF_Baseplate_Collision_Simulation", function()
 		for j = 1, Count do
 			if i >= j then continue end
 			local BP1, BP2 = BaseplatesArray[i], BaseplatesArray[j]
+			local BP1Table, BP2Table = ENTITY.GetTable(BP1), ENTITY.GetTable(BP2)
 
-			if not BP1.Size or not BP2.Size then continue end
-			if BP1:IsPlayerHolding() or BP2:IsPlayerHolding() then continue end
+			if not BP1Table.Size or not BP2Table.Size then continue end
+			if ENTITY.IsPlayerHolding(BP1) or ENTITY.IsPlayerHolding(BP2) then continue end
 
 			local Valid1, Physics1, Pos1, Vel1, Contraption1, PhysMass1, TotalMass1, Radius1 = GetBaseplateProperties(BP1)
 			local Valid2, Physics2, Pos2, Vel2, Contraption2, PhysMass2, TotalMass2, Radius2 = GetBaseplateProperties(BP2)
@@ -69,25 +80,26 @@ hook.Add("Think", "ACF_Baseplate_Collision_Simulation", function()
 			local BP1Force = CollisionForce1 * math.Clamp(PhysMass1 / TotalMass1, 0, 1)
 			local BP2Force = CollisionForce2 * math.Clamp(PhysMass2 / TotalMass2, 0, 1)
 
-			local BP1LinImpulse, BP1AngImpulse = Physics1:CalculateForceOffset(BP1Force, IntersectionCenter)
-			Physics1:ApplyForceCenter(BP1LinImpulse)
-			Physics1:ApplyTorqueCenter(Physics1:LocalToWorldVector(BP1AngImpulse * 2)) -- Are you sure this was a good idea?
+			local BP1LinImpulse, BP1AngImpulse = PHYSOBJ.CalculateForceOffset(Physics1, BP1Force, IntersectionCenter)
+			PHYSOBJ.ApplyForceCenter(Physics1, BP1LinImpulse)
+			PHYSOBJ.ApplyTorqueCenter(Physics1, PHYSOBJ.LocalToWorldVector(Physics1, BP1AngImpulse * 2)) -- Are you sure this was a good idea?
 			BP1:PlayBaseplateRepulsionSound(Vel1)
 
-			local BP2LinImpulse, BP2AngImpulse = Physics2:CalculateForceOffset(BP2Force, IntersectionCenter)
-			Physics2:ApplyForceCenter(BP2LinImpulse)
-			Physics2:ApplyTorqueCenter(Physics2:LocalToWorldVector(BP2AngImpulse * 2)) -- Are you sure this was a good idea?
+			local BP2LinImpulse, BP2AngImpulse = PHYSOBJ.CalculateForceOffset(Physics2, BP2Force, IntersectionCenter)
+			PHYSOBJ.ApplyForceCenter(Physics2, BP2LinImpulse)
+			PHYSOBJ.ApplyTorqueCenter(Physics2, PHYSOBJ.LocalToWorldVector(Physics2, BP2AngImpulse * 2)) -- Are you sure this was a good idea?
 			BP2:PlayBaseplateRepulsionSound(Vel2)
 		end
 	end
 end)
 
 function ENT:PlayBaseplateRepulsionSound(Vel)
-	local Hard = Vel:Length() > 500 and true or false
+	local SelfTbl = ENTITY.GetTable(self)
+	local Hard = VECTOR.Length(Vel) > 500 and true or false
 	local Now  = Clock.CurTime
-	local Prev = self.LastPlayRepulsionSound
+	local Prev = SelfTbl.LastPlayRepulsionSound
 	if Prev and Now - Prev < 0.75 then return end
 
-	self.LastPlayRepulsionSound = Now
-	self:EmitSound(Hard and "MetalVehicle.ImpactHard" or "MetalVehicle.ImpactSoft", 150, math.Rand(0.92, 1.05), 1, CHAN_AUTO, 0, 0)
+	SelfTbl.LastPlayRepulsionSound = Now
+	ENTITY.EmitSound(self, Hard and "MetalVehicle.ImpactHard" or "MetalVehicle.ImpactSoft", 150, math.Rand(0.92, 1.05), 1, CHAN_AUTO, 0, 0)
 end
