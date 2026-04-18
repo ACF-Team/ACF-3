44952a3f2ae366342f3a32024c71b97bbae716a4
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2026-04-18 01:11:19 -0400

    Fix altitude fuze error spam

--- lua/acf/entities/fuzes/altitude.lua
+++ lua/acf/entities/fuzes/altitude.lua
@@ -11,9 +11,10 @@ else
 	end
 
 	function Fuze:GetDetonate(Missile, Guidance)
-		if not self:IsArmed() then return false end
+		if not self:IsArmed() or not Guidance then return false end
 
-		local Target = Guidance.TargetPos or Guidance:GetGuidance(Missile).TargetPos
+		local GuidanceResult = Guidance:GetGuidance(Missile)
+		local Target = Guidance.TargetPos or (GuidanceResult and GuidanceResult.TargetPos)
 		if not Target then return false end
 
 		local TargetElevation = Target.z
