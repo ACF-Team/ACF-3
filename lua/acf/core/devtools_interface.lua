dd2d9055b786e632b20bf232a52d5ec4f313de0f
march <106459595+marchc1@users.noreply.github.com>
2026-04-18 15:45:50 -0700

    Generalize the devtools interface
    easier to use in other addons this way

--- lua/acf/core/devtools_interface.lua
+++ lua/acf/core/devtools_interface.lua
@@ -1,18 +1,21 @@
-local ACF = ACF
-ACF.EventViewer = ACF.EventViewer or {}
-ACF.EntityKeyValues = ACF.EntityKeyValues or {}
+-- Generic interface for ACF-3 Devtools
+-- https://github.com/ACF-Team/ACF-3-DevTools
+local AddonName = "ACF"
+local Addon     = _G[AddonName]
+Addon.EventViewer = Addon.EventViewer or {}
+Addon.EntityKeyValues = Addon.EntityKeyValues or {}
 
-function ACF.EventViewer.Enabled() return false end
-function ACF.EntityKeyValues.Enabled() return false end
+function Addon.EventViewer.Enabled() return false end
+function Addon.EntityKeyValues.Enabled() return false end
 
-hook.Add("Initialize", "ACF_SetupStubForDevTools", function()
-    table.Empty(ACF.EventViewer)
-    table.Empty(ACF.EntityKeyValues)
+hook.Add("Initialize", AddonName .. "_SetupStubForDevTools", function()
+    table.Empty(Addon.EventViewer)
+    table.Empty(Addon.EntityKeyValues)
 
     if not ACF_DevTools then
         -- NOTHING should run event viewer code unless this is enabled
-        function ACF.EventViewer.Enabled() return false end
-        function ACF.EntityKeyValues.Enabled() return false end
+        function Addon.EventViewer.Enabled() return false end
+        function Addon.EntityKeyValues.Enabled() return false end
         -- ^^^ respect this 100%
         -- I don't want to maintain a bunch of stubs here.
     else
@@ -20,9 +23,9 @@ hook.Add("Initialize", "ACF_SetupStubForDevTools", function()
         -- For now, this is just an easier way to handle this.
         -- I would rename it to Available to be more clear, but I don't want to have to
         -- do like, .Available() and .Enabled() in the future...
-        function ACF.EntityKeyValues.Enabled() return true end
+        function Addon.EntityKeyValues.Enabled() return true end
 
-        setmetatable(ACF.EventViewer, {__index = ACF_DevTools.EventViewer})
-        setmetatable(ACF.EntityKeyValues, {__index = ACF_DevTools.EntityKeyValues})
+        setmetatable(Addon.EventViewer, {__index = ACF_DevTools.EventViewer})
+        setmetatable(Addon.EntityKeyValues, {__index = ACF_DevTools.EntityKeyValues})
     end
 end)
