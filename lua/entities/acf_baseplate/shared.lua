04cd31c2e484d7eb0b010900bdae8ac83727a0a0
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2026-04-27 23:07:46 -0400

    Trial doubled baseplate width
    For use with cargo transport vehicles. Tentative if necessary.

--- lua/entities/acf_baseplate/shared.lua
+++ lua/entities/acf_baseplate/shared.lua
@@ -10,7 +10,7 @@ ENT.ACF_PreventArmoring = false
 -- Maps user var name to its type, whether it is client data and type specific arguments (all support defaults?)
 ENT.ACF_UserVars = {
 	["BaseplateType"]       = {Type = "SimpleClass",  ClassName = "BaseplateTypes", Default = "GroundVehicle"},
-	["Width"]               = {Type = "Number",       Min = 36,  Max = 120, Default = 36, Decimals = 2},
+	["Width"]               = {Type = "Number",       Min = 36,  Max = 240, Default = 36, Decimals = 2},
 	["Length"]              = {Type = "Number",       Min = 36,  Max = 480, Default = 36, Decimals = 2},
 	["Thickness"]           = {Type = "Number",       Min = 0.5, Max = 3,   Default = 3,  Decimals = 2},
 	["DisableAltE"]         = {Type = "Boolean",      Default = false},
