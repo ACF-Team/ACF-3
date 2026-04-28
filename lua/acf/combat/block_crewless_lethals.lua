4e564bb538616cf4608d40325539b1d1def46e1b
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2026-04-27 23:49:42 -0400

    Prevent lethal weapons from firing on crewless contraptions
    This only applies if crew are required. This deals with crewless vehicles that often leverage missiles to get around crew and thus g-force requirements.

---
+++ lua/acf/combat/block_crewless_lethals.lua
@@ -0,0 +1,37 @@
+local ACF = ACF
+local Utilities = ACF.Utilities
+local Clock     = Utilities.Clock
+
+ACF.CrewlessLethalQueryTimes = ACF.CrewlessLethalQueryTimes or {}
+local CrewlessLethalQueryTimes = ACF.CrewlessLethalQueryTimes
+
+local function CrewlessLethalCheck(Entity)
+    if ACF.CrewFallbackCoef == 1 then return true end -- Crew effectively disabled
+
+    local Now  = Clock.CurTime
+    local Then = CrewlessLethalQueryTimes[Entity]
+    if (Now - (Then or 0)) <= math.Rand(0.5, 1.5) then
+        -- We don't need to do anything then
+        return true
+    end
+
+    local PreviouslyTracked = CrewlessLethalQueryTimes[Entity] ~= nil
+    CrewlessLethalQueryTimes[Entity] = Now
+    if not PreviouslyTracked then
+        Entity:CallOnRemove("ACF_CleanUpPlayerCrewlessLethalCheck", function(Ent)
+            CrewlessLethalQueryTimes[Ent] = nil
+        end)
+    end
+
+    local Contraption = Entity:CFW_GetContraption()
+    local Crews = Contraption and Contraption.entsbyclass.acf_crew
+    if not Crews or next(Crews) == nil then return false end
+end
+
+hook.Add("ACF_PreFireWeapon", "ACF_PreventCrewlessLethals", function(Ent)
+    local Result = CrewlessLethalCheck(Ent)
+    if not Result then
+        ACF.DisableEntity(Ent, "Player Error", string.format("%s %s.", Ent.PluralName or Ent:GetClass(), "Lethal weapons cannot be used on crewless contraptions"), 5)
+        return false
+    end
+end)
