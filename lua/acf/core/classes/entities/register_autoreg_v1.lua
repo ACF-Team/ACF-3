f98cc4cafc535397f4bf9063292206e70e619bfc
march <106459595+marchc1@users.noreply.github.com>
2026-06-08 13:49:11 -0700

    Lets try a little inversion of control-ish here to ease conversion
    Something like that

--- lua/acf/core/classes/entities/register_autoreg_v1.lua
+++ lua/acf/core/classes/entities/register_autoreg_v1.lua
@@ -2,78 +2,6 @@
 The purpose of this class is to define a class that represents an entity, storing its spawn function as well as registering the arguments attached to the entity with duplicators.
 --]]
 
-local duplicator = duplicator
-local isfunction = isfunction
-local isstring   = isstring
-local istable    = istable
-local unpack     = unpack
-
--- This is just to get us out of the hell that is the class system.
-local Classes    = ACF.Classes
-local Entities   = Classes.Entities
-
---- Table mapping entity class names to their class tables
---- @type table<string, table>
-local Entries    = Entities.Entries or {}
-Entities.Entries = Entries
-
---- Represents the arguments of an entity class (and information about them)
---- Note: For older parts of the code base, restrictions are done outside the API, so that may be empty.
---- @class EntityTable
---- @field Lookup table 		# Maps argument name to true
---- @field Count number 		# The total number of arguments
---- @field List table 			# An array of all arguments
---- @field Restrictions table 	# Maps an argument name to its restrictions (See: ACF_UserVars)
-
---- Represents an entity argument (and its restriction) in the new API
---- @class Restriction
---- @field Type string			# The type of the restriction
---- @field ClientData bool		# Whether this property should be updated from the menu
---- @field Default any			# The default value if none is provided
-
---- Gets the entity table of a certain class
---- If an entity table doesn't exist for the class, it will register one.
---- @param Class table The class to get the entity table from
---- @return EntityTable # The entity table of this class
-local function GetEntityTable(Class)
-	local Data = Entries[Class]
-
-	if not Data then
-		Data = {
-			Lookup       = {},
-			Count        = 0,
-			List         = {},
-			Restrictions = {}
-		}
-
-		Entries[Class] = Data
-	end
-
-	return Data
-end
-
---- Adds arguments to an entity for storage in duplicators
---- The Entity.Lookup, Entity.Count and Entity.List variables allow us to iterate over this information in different ways. 
---- @param Entity EntityTable The entity table to add arguments to
---- @param Arguments any[] # An array of arguments to attach to the entity (usually {...})
---- @return any[] # An array of arguments attached to the entity
-local function AddArguments(Entity, Arguments)
-	local Lookup = Entity.Lookup
-	local Count  = Entity.Count
-	local List   = Entity.List
-
-	for _, V in ipairs(Arguments) do
-		if Lookup[V] then continue end	-- Ignore adding what's already registered
-
-		Count = Count + 1				-- Increment the count of arguments
-		Lookup[V]   = true				-- Index the entity argument as used
-		List[Count] = V					-- Append the entity argument to the list
-	end
-
-	Entity.Count = Count				-- Update the count of arguments
-
-	return List
-end
 
 local UserArgumentTypes = {}
 
@@ -93,7 +21,7 @@ end
 
 --- Adds an argument type and verifier to the ArgumentTypes dictionary.
 --- @param Type string The type of data
-function Entities.AddUserArgumentType(Type)
+local function AddUserArgumentType(Type)
 	if UserArgumentTypes[Type] then return end
 
 	-- Def can contain Validator, PreCopy, PostPaste, Getter
@@ -104,16 +32,7 @@ function Entities.AddUserArgumentType(Type)
 	return Def
 end
 
--- To make some notes on the IsClientData flag:
---[[
-	IsClientData designates that a type is designed to be validated on user entity updates. This is acceptable for almost
-	all types, but some types are different. LinkedEntity/LinkedEntities for example require going through PostEntityPaste
-	due to dependency load order, and so validating them in Verify/Update functions results in receiving incorrect data.
-]]
-
-function Entities.IterateTypes() return pairs(UserArgumentTypes) end
-
-local NumberType = Entities.AddUserArgumentType("Number")
+local NumberType = AddUserArgumentType("Number")
 function NumberType.Validator(Ctx, Value)
 	if not isnumber(Value) then Value = ACF.CheckNumber(Value, Ctx:GetSpec("Default") or 0) end
 
@@ -124,7 +43,7 @@ function NumberType.Validator(Ctx, Value)
 	return Value
 end
 
-local StringType = Entities.AddUserArgumentType("String")
+local StringType = AddUserArgumentType("String")
 function StringType.Validator(Ctx, Value)
 	local Specs = Ctx:GetSpecs()
 	if not isstring(Value) then
@@ -134,7 +53,7 @@ function StringType.Validator(Ctx, Value)
 	return Value
 end
 
-local BooleanType = Entities.AddUserArgumentType("Boolean")
+local BooleanType = AddUserArgumentType("Boolean")
 function BooleanType.Validator(Ctx, Value)
 	local Specs = Ctx:GetSpecs()
 	if not isbool(Value) then
@@ -145,7 +64,7 @@ function BooleanType.Validator(Ctx, Value)
 end
 
 -- These should be removed in a future class rewrite
-local SimpleClassType = Entities.AddUserArgumentType("SimpleClass")
+local SimpleClassType = AddUserArgumentType("SimpleClass")
 function SimpleClassType.Validator(Ctx, Value)
 	local Specs = Ctx:GetSpecs()
 	if not isstring(Value) then
@@ -167,7 +86,7 @@ function SimpleClassType.Getter(self, Ctx, Key)
 	return ACF.Classes[Specs.ClassName].Get(Key)
 end
 
-local GroupClassType = Entities.AddUserArgumentType("GroupClass")
+local GroupClassType = AddUserArgumentType("GroupClass")
 function GroupClassType.Validator(Ctx, Value)
 	local Specs = Ctx:GetSpecs()
 	if not isstring(Value) then
@@ -201,7 +120,7 @@ function GroupClassType.Getter(self, Ctx, Key)
 end
 
 -- Single entity link.
-local LinkedEntityType = Entities.AddUserArgumentType("LinkedEntity")
+local LinkedEntityType = AddUserArgumentType("LinkedEntity")
 LinkedEntityType.IsClientData = false
 
 function LinkedEntityType.Validator(Specs, Value)
@@ -227,7 +146,7 @@ function LinkedEntityType.PostPaste(self, _, Value, CreatedEnts)
 end
 
 -- Entity link LUT where Key == Entity and Value == true.
-local LinkedEntitiesType = Entities.AddUserArgumentType("LinkedEntities")
+local LinkedEntitiesType = AddUserArgumentType("LinkedEntities")
 LinkedEntitiesType.IsClientData = false
 
 function LinkedEntitiesType.Validator(Specs, Value)
@@ -280,10 +199,10 @@ end
 --- Adds extra arguments to a class which has been created via Entities.AutoRegister() (or Entities.Register() with no arguments)
 --- @param Class string A class previously registered as an entity class
 --- @param DataKeys table A key-value table, where key is the name of the data and value defines the type and restrictions of the data.
-function Entities.AddStrictArguments(Class, UserVariables)
+local function AddStrictArguments(Class, UserVariables)
 	if not isstring(Class) then return end
 
-	local Entity    = GetEntityTable(Class)
+	local Entity    = Entities.GetEntityTable(Class)
 
 	local UserVars  = table.GetKeys(UserVariables)
 	local ArgumentNames  = {}
@@ -291,7 +210,7 @@ function Entities.AddStrictArguments(Class, UserVariables)
 
 	for _, v in ipairs(UserVars) do ArgumentNames[#ArgumentNames + 1] = v; Arguments[v] = UserVariables[v] end
 
-	local List      = AddArguments(Entity, ArgumentNames)
+	local List      = Entities.AddArgumentsRaw(Entity, ArgumentNames)
 	AddArgumentRestrictions(Entity, Arguments)
 	return List
 end
@@ -393,7 +312,7 @@ function VerificationContext_MT_methods:ResolveClientData(Key)
 end
 
 local function VerificationContext(Class)
-	local Entity = GetEntityTable(Class)
+	local Entity = Entities.GetEntityTable(Class)
 	return setmetatable({
 		Class        = Class,
 		List         = Entity.List,
@@ -512,7 +431,7 @@ AUTOREG TYPE API (semi-internal...)
 		You do not need to validate untrusted value, it is validated immediately after this delegate is called
 		Is optional, but likely required if you defined PreCopyUserVarDelegate.
 
-	Entities.AddUserArgumentType(TypeName, ValidateUserVarDelegate, PreCopyUserVarDelegate?, PostPasteUserVarDelegate?)
+	AddUserArgumentType(TypeName, ValidateUserVarDelegate, PreCopyUserVarDelegate?, PostPasteUserVarDelegate?)
 ]]
 
 -- NEW CHANGE: ACF_UserData has been split into ACF_UserData and ACF_LiveData. The reason being that
@@ -529,7 +448,7 @@ end
 -- Automatically registers an entity. This MUST be the last line in entity/init.lua for everything to work properly
 -- Can be passed with an ENT table if you have some weird usecase, but auto defaults to _G.ENT
 --- @param ENT table A scripted entity class definition (see https://wiki.facepunch.com/gmod/Structures/ENT)
-function Entities.AutoRegister(ENT)
+function Entities.AutoRegisterV1(ENT)
 	if ENT == nil then ENT = _G.ENT end
 	if not ENT then error("Called Entities.AutoRegister(), but no entity was in the process of being created.") end
 
@@ -538,9 +457,9 @@ function Entities.AutoRegister(ENT)
 	local Class  = string.Split(ENT.Folder, "/"); Class = Class[#Class]
 	ENT.ACF_Class = Class
 
-	local Entity = GetEntityTable(Class)
+	local Entity = Entities.GetEntityTable(Class)
 	local UserVars = ENT.ACF_UserVars or {}
-	Entities.AddStrictArguments(Class, UserVars)
+	AddStrictArguments(Class, UserVars)
 
 	local WireIO   = ACF.Utilities.WireIO
 	local Wire_Inputs, Wire_Outputs = ENT.ACF_WireInputs, ENT.ACF_WireOutputs
@@ -595,7 +514,7 @@ function Entities.AutoRegister(ENT)
 		local LiveData          = EntTable.ACF_LiveData
 		local CustomGetterCache = EntTable.ACF_CustomGetterCache
 
-		local RestrictionSpecs = GetEntityTable(Class).Restrictions[Key]
+		local RestrictionSpecs = Entities.GetEntityTable(Class).Restrictions[Key]
 		local TypeSpecs = UserArgumentTypes[RestrictionSpecs.Type]
 
 		if RestrictionSpecs then
@@ -620,7 +539,7 @@ function Entities.AutoRegister(ENT)
 	--- @param self table The entity to update
 	--- @param ClientData table The client data to use for the update
 	local function UpdateEntityData(self, ClientData)
-		local Entity       = GetEntityTable(Class) -- THE ENTITY TABLE, NOT THE ENTITY ITSELF
+		local Entity       = Entities.GetEntityTable(Class) -- THE ENTITY TABLE, NOT THE ENTITY ITSELF
 		local List         = Entity.List
 
 		if self.ACF_PreUpdateEntityData then self:ACF_PreUpdateEntityData(ClientData) end
@@ -908,192 +827,3 @@ function Entities.AutoRegister(ENT)
 	duplicator.RegisterEntityClass(Class, SpawnFunction, "Pos", "Angle", "ACF_UserData", unpack(ReadKeys))
 end
 
---- Registers a class as a spawnable entity class
---- @param Class string The class to register
---- @param Function fun(Player:entity, Pos:vector, Ang:angle, Data:table):Entity A function defining how to spawn your class (This should be your ACF.Make<something> function)
---- @param ... any #A vararg of arguments to attach to the entity
-function Entities.Register(Class, Function, ...)
-	if Class == nil and Function == nil then
-		-- Calling Entities.Register with no arguments performs an automatic registration
-		Entities.AutoRegister(ENT)
-		return
-	end
-
-	if not isstring(Class) then return end
-	if not isfunction(Function) then return end
-
-	local Entity    = GetEntityTable(Class)
-	local Arguments = istable(...) and ... or { ... }
-	local List      = AddArguments(Entity, Arguments)
-
-	Entity.Spawn = Function
-
-	local function SpawnFunction(Player, Pos, Angle, Data)
-		local _, SpawnedEntity = Entities.Spawn(Class, Player, Pos, Angle, Data, true)
-
-		return SpawnedEntity
-	end
-
-	duplicator.RegisterEntityClass(Class, SpawnFunction, "Pos", "Angle", "Data", unpack(List))
-end
-
---- Adds extra arguments to a class which has already been called in Entities.Register  
---- Should be called after Entities.Register if you want to specify any additional arguments
---- @param Class string A class previously registered as an entity class
---- @param ... any #A vararg of arguments
-function Entities.AddArguments(Class, ...)
-	if not isstring(Class) then return end
-
-	local Entity    = GetEntityTable(Class)
-	local Arguments = istable(...) and ... or { ... }
-	local List      = AddArguments(Entity, Arguments)
-
-	if Entity.Spawn then
-		local function SpawnFunction(Player, Pos, Angle, Data)
-			local _, SpawnedEntity = Entities.Spawn(Class, Player, Pos, Angle, Data, true)
-
-			return SpawnedEntity
-		end
-
-		duplicator.RegisterEntityClass(Class, SpawnFunction, "Pos", "Angle", "Data", unpack(List))
-	end
-end
-
---- Returns an array of the entity's arguments
---- @param Class string The entity class to get arguments from
---- @return any[] # An array of arguments attached to the entity
-function Entities.GetArguments(Class)
-	if not isstring(Class) then return end
-
-	local Entity = GetEntityTable(Class)
-	local List   = {}
-
-	for K, V in ipairs(Entity.List) do
-		List[K] = V
-	end
-
-	return List
-end
-
--- Entity classes use the simple class system
--- As we are trying to deprecate it, the code for that has been repeated here.
--- In the future, this file should just be refactored entirely.
-
---- Gets the entry from the namespace with the given ID
---- @param ID string The ID of the entry
---- @return table | nil # The entry
-function Entities.Get(ID) return isstring(ID) and Entries[ID] or nil end
-
---- Gets all the entries in the namespace  
---- If aliases exist in the namespace, they will be ignored in the returned table
---- @return table<string,table> # A table mapping the an entry's ID to itself 
-function Entities.GetEntries()
-	local Result = {}
-	for _, V in pairs(Entries) do Result[V.ID] = V end
-	return Result
-end
-
---- Gets the stored entries table
---- Returns the original reference
---- Allows the class system to restore itself later
---- @return table # The stored entries table
-function Entities.GetStored() return Entries end
-
---- Gets all the entries in the namespace  
---- If aliases exist in the namespace, they will be included in the returned table
---- @return table<number,table> # An "array" (e.g. {class1,class2,...}) containing entries
-function Entities.GetList()
-	local Result = {}
-	local Count  = 0
-
-	for _, V in pairs(Entries) do
-		Count = Count + 1
-
-		Result[Count] = V
-	end
-
-	return Result
-end
-
-if CLIENT then return end
-
-do -- Spawning and updating
-	local undo = undo
-
-	--- Spawns an entity with the given parameters
-	--- Internally calls the class' Spawn method
-	--- @param Class string The class of entity to spawn
-	--- @param Player entity The player creating the entity
-	--- @param Position vector The position to create the entity at
-	--- @param Angles angle The angles to create the entity at
-	--- @param Data table The data to pass into the entity's spawn function
-	--- @param NoUndo boolean Whether the entity is added to the undo list (can be z keyed)
-	--- @return boolean, table? # Whether the spawning was successful and the reason why
-	function Entities.Spawn(Class, Player, Position, Angles, Data, NoUndo)
-		if not isstring(Class) then return false end
-
-		local ClassData = Entities.Get(Class)
-
-		if not ClassData then return false, Class .. " is not a registered ACF entity class." end
-		if not ClassData.Spawn then return false, Class .. " doesn't have a spawn function assigned to it." end
-
-		local Entity = ClassData.Spawn(Player, Position, Angles, Data)
-
-		if not IsValid(Entity) then return false, "The spawn function for " .. Class .. " didn't return an entity." end
-
-		Entity:CPPISetOwner(Player)
-		Entity:SetPlayer(Player)
-
-		Entity.ACF       = Entity.ACF or {}
-		Entity.Owner     = Player -- MUST be stored on ent for PP (supposedly)
-		Entity.DataStore = Entities.GetArguments(Class)
-
-		if not NoUndo then
-			undo.Create(Entity.Name or Class)
-				undo.AddEntity(Entity)
-				undo.SetPlayer(Player)
-			undo.Finish()
-		end
-
-		if Entity.UpdateOverlay then
-			Entity:UpdateOverlay(true)
-		end
-
-		if Entity.Outputs and Entity.Outputs.Entity then
-			WireLib.TriggerOutput(Entity, "Entity", Entity)
-		end
-
-		ACF.CheckLegal(Entity)
-
-		return true, Entity
-	end
-
-	--- Triggers the update function of an entity  
-	--- Internally calls the ENT:Update(Data) metamethod that's implemented on all entities
-	--- @param Entity table The entity to update
-	--- @param Data table The data to pass into the entity on update
-	--- @return boolean, string # Whether the update was successful and the reason why
-	function Entities.Update(Entity, Data)
-		if not IsValid(Entity) then return false, "Can't update invalid entities." end
-		if not isfunction(Entity.Update) then return false, "This entity does not support updating." end
-
-		Data = istable(Data) and Data or {}
-
-		local Result, Message = Entity:Update(Data)
-
-		if Result then
-			if Entity.UpdateOverlay then
-				Entity:UpdateOverlay(true)
-			end
-
-			-- Let the client know that we've updated this entity
-			net.Start("ACF_UpdateEntity")
-				net.WriteEntity(Entity)
-			net.Broadcast()
-		else
-			Message = "Couldn't update entity: " .. (Message or "No reason provided.")
-		end
-
-		return Result, Message
-	end
-end
---
+++ lua/acf/core/classes/entities/register_legacy.lua
@@ -0,0 +1,184 @@
+local Classes    = ACF.Classes
+local Entities   = Classes.Entities
+
+--- Table mapping entity class names to their class tables
+--- @type table<string, table>
+local Entries    = {}
+
+--- Represents the arguments of an entity class (and information about them)
+--- @class EntityTable
+--- @field Lookup table 		# Maps argument name to true
+--- @field Count number 		# The total number of arguments
+--- @field List table 			# An array of all arguments
+--- @field Restrictions table 	# Maps an argument name to its restrictions (See: ACF_UserVars)
+
+--- Represents an entity argument (and its restriction) in the new API
+--- @class Restriction
+--- @field Type string			# The type of the restriction
+--- @field ClientData bool		# Whether this property should be updated from the menu
+--- @field Default any			# The default value if none is provided
+
+--- Gets the entity table of a certain class
+--- If an entity table doesn't exist for the class, it will register one.
+--- @param Class table The class to get the entity table from
+--- @return EntityTable # The entity table of this class
+local function GetEntityTable(Class)
+    local Data = Entries[Class]
+
+    if not Data then
+        Data = {
+            Lookup       = {},
+            Count        = 0,
+            List         = {},
+            Restrictions = {}
+        }
+
+        Entries[Class] = Data
+    end
+
+    return Data
+end
+Entities.GetEntityTable = GetEntityTable -- for autoreg V1
+
+
+--- Adds arguments to an entity for storage in duplicators
+--- The Entity.Lookup, Entity.Count and Entity.List variables allow us to iterate over this information in different ways. 
+--- @param Entity EntityTable The entity table to add arguments to
+--- @param Arguments any[] # An array of arguments to attach to the entity (usually {...})
+--- @return any[] # An array of arguments attached to the entity
+local function AddArguments(Entity, Arguments)
+    local Lookup = Entity.Lookup
+    local Count  = Entity.Count
+    local List   = Entity.List
+
+    for _, V in ipairs(Arguments) do
+        if Lookup[V] then continue end	-- Ignore adding what's already registered
+
+        Count = Count + 1				-- Increment the count of arguments
+        Lookup[V]   = true				-- Index the entity argument as used
+        List[Count] = V					-- Append the entity argument to the list
+    end
+
+    Entity.Count = Count				-- Update the count of arguments
+
+    return List
+end
+Entities.AddArgumentsRaw = AddArguments
+
+--- Adds extra arguments to a class which has already been called in Entities.Register  
+--- Should be called after Entities.Register if you want to specify any additional arguments
+--- @param Class string A class previously registered as an entity class
+--- @param ... any #A vararg of arguments
+function Entities.AddArguments(Class, ...)
+    if not isstring(Class) then return end
+
+    local Entity    = GetEntityTable(Class)
+    local Arguments = istable(...) and ... or { ... }
+    local List      = AddArguments(Entity, Arguments)
+
+    if Entity.Spawn then
+        local function SpawnFunction(Player, Pos, Angle, Data)
+            local _, SpawnedEntity = Entities.Spawn(Class, Player, Pos, Angle, Data, true)
+
+            return SpawnedEntity
+        end
+
+        duplicator.RegisterEntityClass(Class, SpawnFunction, "Pos", "Angle", "Data", unpack(List))
+    end
+end
+
+--- Returns an array of the entity's arguments
+--- @param Class string The entity class to get arguments from
+--- @return any[] # An array of arguments attached to the entity
+function Entities.GetArguments(Class)
+    if not isstring(Class) then return end
+
+    local Entity = GetEntityTable(Class)
+    local List   = {}
+
+    for K, V in ipairs(Entity.List) do
+        List[K] = V
+    end
+
+    return List
+end
+
+-- Entity classes use the simple class system
+-- As we are trying to deprecate it, the code for that has been repeated here.
+-- In the future, this file should just be refactored entirely.
+
+--- Gets the entry from the namespace with the given ID
+--- @param ID string The ID of the entry
+--- @return table | nil # The entry
+function Entities.Get(ID) return isstring(ID) and Entries[ID] or nil end
+
+--- Gets all the entries in the namespace  
+--- If aliases exist in the namespace, they will be ignored in the returned table
+--- @return table<string,table> # A table mapping the an entry's ID to itself 
+function Entities.GetEntries()
+    local Result = {}
+    for _, V in pairs(Entries) do Result[V.ID] = V end
+    return Result
+end
+
+--- Gets the stored entries table
+--- Returns the original reference
+--- Allows the class system to restore itself later
+--- @return table # The stored entries table
+function Entities.GetStored() return Entries end
+
+--- Gets all the entries in the namespace  
+--- If aliases exist in the namespace, they will be included in the returned table
+--- @return table<number,table> # An "array" (e.g. {class1,class2,...}) containing entries
+function Entities.GetList()
+    local Result = {}
+    local Count  = 0
+
+    for _, V in pairs(Entries) do
+        Count = Count + 1
+
+        Result[Count] = V
+    end
+
+    return Result
+end
+
+--- Registers a class as a spawnable entity class
+--- @param Class string The class to register
+--- @param Function fun(Player:entity, Pos:vector, Ang:angle, Data:table):Entity A function defining how to spawn your class (This should be your ACF.Make<something> function)
+--- @param ... any #A vararg of arguments to attach to the entity
+function Entities.Register(Class, Function, ...)
+    if Class == nil and Function == nil then
+        -- Calling Entities.Register with no arguments performs an automatic registration
+        Entities.AutoRegister(ENT)
+        return
+    end
+
+    if not isstring(Class) then return end
+    if not isfunction(Function) then return end
+
+    local Entity    = GetEntityTable(Class)
+    local Arguments = istable(...) and ... or { ... }
+    local List      = AddArguments(Entity, Arguments)
+
+    Entity.Spawn = Function
+
+    local function SpawnFunction(Player, Pos, Angle, Data)
+        local _, SpawnedEntity = Entities.Spawn(Class, Player, Pos, Angle, Data, true)
+
+        return SpawnedEntity
+    end
+
+    duplicator.RegisterEntityClass(Class, SpawnFunction, "Pos", "Angle", "Data", unpack(List))
+end
+
+-- Autoreg also gets handled this way, since it all uses the same legacy backbone
+hook.Add("ACF_TemporaryHook_InstantiateEntity", "Legacy", function(Class, Player, Position, Angles, Data)
+    local ClassData = Entities.Get(Class)
+
+    if not ClassData then return end
+    if not ClassData.Spawn then return end
+
+    local Entity = ClassData.Spawn(Player, Position, Angles, Data)
+    if IsValid(Entity) then return Entity end
+end)
