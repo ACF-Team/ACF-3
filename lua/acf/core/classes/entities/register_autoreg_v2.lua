9edbba26321f22657a830b6158a59ddbc713051b
march <106459595+marchc1@users.noreply.github.com>
2026-06-08 15:51:15 -0700

    Start autoreg v2, new classes

--- lua/acf/core/classes/entities/register_autoreg_v2.lua
+++ lua/acf/core/classes/entities/register_autoreg_v2.lua
@@ -1,1 +1,63 @@
--- todo
+-- Define base entity types that will never get these ran just so they exist
+ACF.Classes.DefineClass("base_wire_entity",                       function() end)
+ACF.Classes.DefineClass("base_scalable",      "base_wire_entity", function() end)
+ACF.Classes.DefineClass("acf_base_simple",    "base_wire_entity", function() end)
+ACF.Classes.DefineClass("acf_base_scalable",  "base_scalable",    function() end)
+
+local function ClassNameTrick(ENT)
+    local Class  = string.Split(ENT.Folder, "/"); Class = Class[#Class]
+    ENT.ACF_ClassName = Class
+    ENT.ACF_BaseClassName = ENT.Base
+end
+
+local function ClassFieldDefinitions(ENT, DefineFields)
+    ENT.ACF_ClassDef = ACF.Classes.DefineClass(ENT.ACF_ClassName, ENT.ACF_BaseClass, DefineFields)
+end
+
+--- This sets up wiremod functions
+local function PrepareWiremodFunctions(ENT)
+    local Wire_Inputs, Wire_Outputs = ENT.ACF_StaticWireInputs or {}, ENT.ACF_StaticWireOutputs or {}
+
+    -- Internal call
+    function ENT:ACF_SetupWireFunctions()
+        local Inputs, Outputs = {}, {}
+        for K, V in ipairs(Wire_Inputs) do Inputs[K] = V end
+        for K, V in ipairs(Wire_Outputs) do Outputs[K] = V end
+
+        self:ACF_SetupWireIO(Inputs, Outputs)
+
+        if Wire_Inputs then
+            if Entity.Inputs then
+                Entity.Inputs = WireLib.AdjustInputs(Entity, Inputs)
+            else
+                Entity.Inputs = WireLib.CreateInputs(Entity, Inputs)
+            end
+        end
+
+        if Wire_Outputs then
+            if Entity.Outputs then
+                Entity.Outputs = WireLib.AdjustOutputs(Entity, Outputs)
+            else
+                Entity.Outputs = WireLib.CreateOutputs(Entity, Outputs)
+            end
+        end
+    end
+
+    -- ACF SENT hook
+    if not ENT.ACF_SetupWireIO then ENT.ACF_SetupWireIO = function() end end
+end
+
+function ACF.AutoRegisterV2(DefineFields)
+    ClassNameTrick(ENT)
+    ClassFieldDefinitions(ENT, DefineFields)
+
+    local ExpectedClass = ACF.ClassName
+
+    local Idx = "ACF.AutoRegister" .. SysTime()
+    hook.Add("PreRegisterSENT", Idx, function(ENT, Class)
+        hook.Remove("PreRegisterSENT", Idx)
+
+        if Class ~= ExpectedClass then return end
+        PrepareWiremodFunctions(ENT)
+    end)
+end
--- lua/entities/acf_baseplate/shared.lua
+++ lua/entities/acf_baseplate/shared.lua
@@ -1,30 +1,29 @@
 DEFINE_BASECLASS "acf_base_scalable"
 
-ENT.PrintName      = "ACF Baseplate"
-ENT.WireDebugName  = "ACF Baseplate"
-ENT.PluralName     = "ACF Baseplates"
-ENT.IsACFBaseplate = true
-ENT.ACF_Limit      = 2
+ENT.PrintName      		= "ACF Baseplate"
+ENT.WireDebugName  		= "ACF Baseplate"
+ENT.PluralName     		= "ACF Baseplates"
+ENT.IsACFBaseplate 		= true
+ENT.ACF_Limit      		= 2
 ENT.ACF_PreventArmoring = false
 
--- Maps user var name to its type, whether it is client data and type specific arguments (all support defaults?)
-ENT.ACF_UserVars = {
-	["BaseplateType"]       = {Type = "SimpleClass",  ClassName = "BaseplateTypes", Default = "GroundVehicle"},
-	["Width"]               = {Type = "Number",       Min = 36,  Max = 240, Default = 36, Decimals = 2},
-	["Length"]              = {Type = "Number",       Min = 36,  Max = 480, Default = 36, Decimals = 2},
-	["Thickness"]           = {Type = "Number",       Min = 0.5, Max = 3,   Default = 3,  Decimals = 2},
-	["DisableAltE"]         = {Type = "Boolean",      Default = false},
-	["ExplodeOnCollisions"] = {Type = "Boolean",      Default = false},
-	["Seat"]                = {Type = "LinkedEntity", Classes = {prop_vehicle_prisoner_pod = true}},
-	["AlreadyHasSeat"]      = {Type = "Boolean",      Default = false, IsClientData = false},
-	["GForceTicks"]         = {Type = "Number",       Min = 1,   Max = 7,   Default = 1,  Decimals = 0},
-}
+ACF.AutoRegisterV2(function()
+	MENU_FIELD("ACF.Baseplates.BaseplateType", "BaseplateType", 			{InstantiateTypeForDefault = "ACF.Baseplates.GroundVehicle"})
+	MENU_FIELD("Number",  					  	"Width", 					{Min = 36,  Max = 240, Default = 36, Decimals = 2})
+	MENU_FIELD("Number",  					  	"Width", 					{Min = 36,  Max = 480, Default = 36, Decimals = 2})
+	MENU_FIELD("Number",  					  	"Thickness",				{Min = 0.5,  Max = 3, Default = 3, Decimals = 2})
+	MENU_FIELD("Boolean", 					  	"DisableAltE",    			{Default = false})
+	MENU_FIELD("Boolean", 					  	"ExplodeOnCollisions",    	{Default = false})
+		 FIELD("Seat", 					  		"Entity",    				{AcceptableClasses = {prop_vehicle_prisoner_pod = true}})
+		 FIELD("Boolean", 					  	"AlreadyHasSeat",    		{Default = false})
+	MENU_FIELD("Number",  					  	"GForceTicks",				{Min = 1,  Max = 7, Default = 1, Decimals = 0})
+end)
 
-ENT.ACF_WireInputs = {
+ENT.ACF_StaticWireInputs = {
 	"Unflip (Triggers an unflip on the baseplate)",
 }
 
-ENT.ACF_WireOutputs = {
+ENT.ACF_StaticWireOutputs = {
 	"Entity (The entity itself) [ENTITY]",
 	"Vehicles (Seat for this entity, compatible with wire) [ARRAY]",
 }
