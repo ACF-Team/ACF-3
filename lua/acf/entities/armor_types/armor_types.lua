7b4b5b57c4c3ebb20c5ddc1f3868096f00a693c9
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2026-06-11 01:32:57 -0400

    Add sources to armor
    I tried

--- lua/acf/entities/armor_types/armor_types.lua
+++ lua/acf/entities/armor_types/armor_types.lua
@@ -24,7 +24,7 @@ local Armor = Types.Register("Aluminum")
 function Armor:OnLoaded()
     self.Name        = "Aluminum"
     self.Description = "Lightweight but weak armor."
-    self.Density     = 2.7e-3
+    self.Density     = 2.7e-3 -- https://en.wikipedia.org/wiki/Aluminium
     self.HealthMul   = 0.8
     self.KineticMul  = 0.5
     self.ChemicalMul = 0.6
@@ -36,7 +36,7 @@ local Armor = Types.Register("RHA")
 function Armor:OnLoaded()
     self.Name        = "RHA"
     self.Description = "Rolled Homogeneous Armor, balanced protection."
-    self.Density     = 7.84e-3
+    self.Density     = 7.84e-3 -- https://metalzenith.com/blogs/steel-properties/rha-steel-properties-and-key-applications-in-defense
     self.HealthMul   = 1.0
     self.KineticMul  = 1.0
     self.ChemicalMul = 1.0
@@ -48,7 +48,7 @@ local Armor = Types.Register("Rubber")
 function Armor:OnLoaded()
     self.Name        = "Rubber"
     self.Description = "Flexible but offers minimal protection."
-    self.Density     = 1.5e-3
+    self.Density     = 1.5e-3 -- * https://rubberandseal.com/what-is-the-density-of-rubber-sheets/
     self.HealthMul   = 1.5
     self.KineticMul  = 0.2
     self.ChemicalMul = 0.3
@@ -60,7 +60,7 @@ local Armor = Types.Register("Textolite")
 function Armor:OnLoaded()
     self.Name        = "Textolite"
     self.Description = "Composite material, good for lightweight applications."
-    self.Density     = 1.8e-3
+    self.Density     = 1.8e-3 -- * http://www.china-anza.com/2-1-7-textolite-3025.html
     self.HealthMul   = 1.0
     self.KineticMul  = 0.4
     self.ChemicalMul = 0.5
@@ -72,7 +72,7 @@ local Armor = Types.Register("Tungsten")
 function Armor:OnLoaded()
     self.Name        = "Tungsten"
     self.Description = "Very dense and strong, but heavy."
-    self.Density     = 19.25e-3
+    self.Density     = 19.25e-3 -- https://en.wikipedia.org/wiki/Tungsten
     self.HealthMul   = 0.7
     self.KineticMul  = 1.5
     self.ChemicalMul = 1.2
@@ -84,7 +84,7 @@ local Armor = Types.Register("DU")
 function Armor:OnLoaded()
     self.Name        = "Depleted Uranium"
     self.Description = "Extremely dense with high protection."
-    self.Density     = 18.7e-3
+    self.Density     = 18.7e-3 -- https://pubmed.ncbi.nlm.nih.gov/11218253/
     self.HealthMul   = 0.9
     self.KineticMul  = 1.3
     self.ChemicalMul = 1.1
@@ -96,7 +96,7 @@ local Armor = Types.Register("LightERA")
 function Armor:OnLoaded()
     self.Name        = "Light ERA"
     self.Description = "Explosive Reactive Armor, effective against shaped charges."
-    self.Density     = 5e-3
+    self.Density     = 5e-3 -- * https://below-the-turret-ring.blogspot.com/2016/04/explosive-reactive-armor-some-history.html
     self.HealthMul   = 0.5
     self.KineticMul  = 0.3
     self.ChemicalMul = 3.0
@@ -108,7 +108,7 @@ local Armor = Types.Register("HeavyERA")
 function Armor:OnLoaded()
     self.Name        = "Heavy ERA"
     self.Description = "Heavy Explosive Reactive Armor, offers some protection against kinetic threats too."
-    self.Density     = 10e-3
+    self.Density     = 10e-3 -- * https://below-the-turret-ring.blogspot.com/2016/04/explosive-reactive-armor-some-history.html
     self.HealthMul   = 0.6
     self.KineticMul  = 0.6
     self.ChemicalMul = 2.0
