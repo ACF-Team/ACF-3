28df3f86a017911df12d9293d5e6809f3697ec3f
march <106459595+marchc1@users.noreply.github.com>
2026-06-14 04:38:42 -0700

    Try converting ground loaders

--- lua/entities/acf_groundloader/shared.lua
+++ lua/entities/acf_groundloader/shared.lua
@@ -3,12 +3,12 @@ DEFINE_BASECLASS "acf_base_simple"
 ENT.PrintName     = "ACF Ground Loader"
 ENT.WireDebugName = "ACF Ground Loader"
 ENT.PluralName    = "ACF Ground Loaders"
-ENT.IsACFEntity = true
 ENT.IsACFBaseplate = true
+ENT.ACF_Limit     = 2
 
-ENT.ACF_UserVars = {
-    ["LinkedAmmoCrates"] = {Type = "LinkedEntities", Classes = {acf_ammo = true}},
-}
+ACF.AutoRegisterV2(function()
+	LINKED_ENTITY_ARRAY_FIELD("LinkedAmmoCrates", { AcceptableClasses = { acf_ammo = true } })
+end)
 
 local LoadingRadius, ReceivingRadius = 256, 128
 return LoadingRadius, ReceivingRadius
