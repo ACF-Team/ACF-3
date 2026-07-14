d08abadaf0911af2a54175d384a73abec781cb57
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2026-07-14 01:48:09 -0400

    Add options for baseplate entry
    Opt out of network optimizations (hiding wheels) or unfreezing physical entities

--- lua/entities/acf_baseplate/shared.lua
+++ lua/entities/acf_baseplate/shared.lua
@@ -14,6 +14,8 @@ ENT.ACF_UserVars = {
 	["Length"]              = {Type = "Number",       Min = 36,  Max = 480, Default = 36, Decimals = 2},
 	["Thickness"]           = {Type = "Number",       Min = 0.5, Max = 3,   Default = 3,  Decimals = 2},
 	["DisableAltE"]         = {Type = "Boolean",      Default = false},
+	["NetworkOptimization"] = {Type = "Boolean",      Default = true},
+	["UnfreezeOnEntry"]     = {Type = "Boolean",      Default = false},
 	["ExplodeOnCollisions"] = {Type = "Boolean",      Default = false},
 	["Seat"]                = {Type = "LinkedEntity", Classes = {prop_vehicle_prisoner_pod = true}},
 	["AlreadyHasSeat"]      = {Type = "Boolean",      Default = false, IsClientData = false},
