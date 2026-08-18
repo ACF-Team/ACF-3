c66ece9df290dd87cf30a803773416548bf01e89
march <106459595+marchc1@users.noreply.github.com>
2026-08-18 15:30:28 -0700

    GetLegacyStyleClassName in AIO code

--- lua/entities/acf_controller/modules/hud.lua
+++ lua/entities/acf_controller/modules/hud.lua
@@ -23,7 +23,7 @@ do
 		-- Network various statistics
 		if IsValid(SelfTbl.Primary) then
 			RecacheBindNW(self, SelfTbl, "AHS_Primary_SL", SelfTbl.Primary.TotalAmmo or 0, self.SetNWInt)
-			RecacheBindNW(self, SelfTbl, "AHS_Primary_AT", ACF.Classes.FullyQualifiedNameToUnqualifiedName(SelfTbl.Primary.BulletData.AmmoType), self.SetNWString)
+			RecacheBindNW(self, SelfTbl, "AHS_Primary_AT", ACF.GetLegacyStyleClassName(SelfTbl.Primary.BulletData.AmmoType), self.SetNWString)
 			RecacheBindNW(self, SelfTbl, "AHS_Primary_NF", SelfTbl.Primary.NextFire or 0, self.SetNWFloat)
 			RecacheBindNW(self, SelfTbl, "AHS_Primary_RT", SelfTbl.Primary.ReloadTime or 0, self.SetNWFloat)
 			RecacheBindNW(self, SelfTbl, "AHS_Primary_RD", SelfTbl.Primary.State == "Loaded", self.SetNWBool)
@@ -35,7 +35,7 @@ do
 
 		if IsValid(SelfTbl.Secondary) then
 			RecacheBindNW(self, SelfTbl, "AHS_Secondary_SL", SelfTbl.Secondary.TotalAmmo or 0, self.SetNWInt)
-			RecacheBindNW(self, SelfTbl, "AHS_Secondary_AT", ACF.Classes.FullyQualifiedNameToUnqualifiedName(SelfTbl.Secondary.BulletData.AmmoType), self.SetNWString)
+			RecacheBindNW(self, SelfTbl, "AHS_Secondary_AT", ACF.GetLegacyStyleClassName(SelfTbl.Secondary.BulletData.AmmoType), self.SetNWString)
 			RecacheBindNW(self, SelfTbl, "AHS_Secondary_NF", SelfTbl.Secondary.NextFire or 0, self.SetNWFloat)
 			RecacheBindNW(self, SelfTbl, "AHS_Secondary_RT", SelfTbl.Secondary.ReloadTime or 0, self.SetNWFloat)
 			RecacheBindNW(self, SelfTbl, "AHS_Secondary_RD", SelfTbl.Secondary.State == "Loaded", self.SetNWBool)
@@ -46,7 +46,7 @@ do
 
 		if IsValid(SelfTbl.Tertiary) then
 			RecacheBindNW(self, SelfTbl, "AHS_Tertiary_SL", SelfTbl.Tertiary.TotalAmmo or 0, self.SetNWInt)
-			RecacheBindNW(self, SelfTbl, "AHS_Tertiary_AT", ACF.Classes.FullyQualifiedNameToUnqualifiedName(SelfTbl.Tertiary.BulletData.AmmoType), self.SetNWString)
+			RecacheBindNW(self, SelfTbl, "AHS_Tertiary_AT", ACF.GetLegacyStyleClassName(SelfTbl.Tertiary.BulletData.AmmoType), self.SetNWString)
 			RecacheBindNW(self, SelfTbl, "AHS_Tertiary_NF", SelfTbl.Tertiary.NextFire or 0, self.SetNWFloat)
 			RecacheBindNW(self, SelfTbl, "AHS_Tertiary_RT", SelfTbl.Tertiary.ReloadTime or 0, self.SetNWFloat)
 			RecacheBindNW(self, SelfTbl, "AHS_Tertiary_RD", SelfTbl.Tertiary.State == "Loaded", self.SetNWBool)
