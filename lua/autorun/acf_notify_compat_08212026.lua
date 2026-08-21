f5d677268363f0eb11a4fdfea5f0b1a2eb4f3d50
march <106459595+marchc1@users.noreply.github.com>
2026-08-21 10:54:44 -0700

    Make that notification look a little prettier

--- lua/autorun/acf_notify_compat_08212026.lua
+++ lua/autorun/acf_notify_compat_08212026.lua
@@ -7,7 +7,7 @@ local function ShowACFWarning()
 
     local frame = vgui.Create("DFrame")
     frame:SetTitle("ACF - Compatibility Warning")
-    frame:SetSize(ScrW() / 4, ScrH() / 4)
+    frame:SetSize(ScrW() / 3, ScrH() / 3)
     frame:Center()
     frame:MakePopup()
     frame:SetSizable(false)
@@ -15,23 +15,28 @@ local function ShowACFWarning()
     local label = vgui.Create("DLabel", frame)
     label:SetPos(15, 35)
     label:SetSize(490, 100)
+    label:SetFont("Trebuchet18")
+    label:Dock(FILL)
+    label:DockMargin(32, 32, 32, 32)
     label:SetWrap(true)
-    label:SetContentAlignment(7)
+    label:SetContentAlignment(5)
     label:SetText([[This server is on the dev branch, which recently had vast internal changes. 
-    Your builds on this branch, if saved to a dupe, will not work on master until dev is merged into master. 
-    Please don't work on anything that you immediately want to use on the master version of the addon.
+Your builds on this branch, if saved to a dupe, will not work on master until dev is merged into master. 
+Please don't work on anything that you immediately want to use on the master version of the addon.
 
-    If you run into issues, please report them on the GitHub (https://github.com/ACF-Team/ACF-3), providing reproduction steps (if possible), symptoms of the problem, and stack tracebacks (if available)]])
-
-    local checkbox = vgui.Create("DCheckBoxLabel", frame)
-    checkbox:SetText("Don't show this again, and provide an in game notification when dev has been merged into master")
-    checkbox:SizeToContents()
-    checkbox:SetPos(frame:GetWide() - checkbox:GetWide() - 15, 140)
+If you run into issues, please report them on the GitHub (https://github.com/ACF-Team/ACF-3), providing reproduction steps (if possible), symptoms of the problem, and stack tracebacks (if available)]])
 
     local closeBtn = vgui.Create("DButton", frame)
     closeBtn:SetText("Close")
     closeBtn:SetSize(100, 25)
-    closeBtn:SetPos((frame:GetWide() / 2) - 50, 170)
+    closeBtn:DockMargin(4, 4, 4, 4)
+    closeBtn:Dock(BOTTOM)
+
+    local checkbox = vgui.Create("DCheckBoxLabel", frame)
+    checkbox:SetText("Don't show this again, and provide an in game notification when dev has been merged into master")
+    checkbox:SizeToContents()
+    checkbox:Dock(BOTTOM)
+    checkbox:DockMargin(4, 4, 4, 4)
 
     closeBtn.DoClick = function()
         if checkbox:GetChecked() then
