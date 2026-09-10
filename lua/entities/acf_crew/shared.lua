123afcf398b51a496b32c358895f421d899b7ab8
march <106459595+marchc1@users.noreply.github.com>
2026-09-10 09:32:35 -0700

    Fix callers

--- lua/entities/acf_crew/shared.lua
+++ lua/entities/acf_crew/shared.lua
@@ -1,6 +1,6 @@
 DEFINE_BASECLASS("acf_base_simple")
 
-ACF.Entities.AutoRegisterV2(function(CLASS)
+ACF.Entities.AutoRegister(2026091001, function(CLASS)
 	MENU_FIELD("String",  "CrewTypeID",                {Default = "Commander"})
 	MENU_FIELD("String",  "CrewModelID",               {Default = "Sitting"})
 	MENU_FIELD("String",  "CrewPoseID",                {Default = ""})
