6ced659903e605a82127ec24d9c7d88d62a2136a
LengthenedGradient <109800352+LengthenedGradient@users.noreply.github.com>
2026-09-11 22:16:30 -0400

    Fix previous

--- lua/acf/contraption/track_cost_sv.lua
+++ lua/acf/contraption/track_cost_sv.lua
@@ -245,6 +245,7 @@ do -- Cost limit enforcement
 
 		for Entity in pairs(Contraption.ents) do
 			if not IsValid(Entity) then continue end
+			if Entity.IsACFEntity then continue end
 
 			local MeshData = Entity.ACF_Volumetric_Mesh
 			if not MeshData then continue end
